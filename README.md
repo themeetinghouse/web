e3a96fa4f558abb334d90ccd9b25e37998ac2349
George Bell <georgebell@George-Bells-MacBook-Pro.local>
2019-05-02 01:09:28 -0400

    Change folders

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # The Meeting House Website
 To run the admin site:
 
-1. amplify publish
-2. npm run-script dev
+1. brew install eb
+2. cd web
+3. amplify publish
+4. npm run-script dev
+5. eb create
+6. eb push
+
 
--- ssr-server.js
+++ ssr-server.js
@@ -49,9 +49,9 @@ app.prepare()
       return handle(req, res)
     })
       
-    server.listen(3000, (err) => {
+    server.listen(8081, (err) => {
       if (err) throw err
-      console.log('> Ready on http://localhost:3000')
+      console.log('> Ready on http://localhost:8081')
     })
   })
 
