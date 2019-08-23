fc0fa2056f9ea1fae68bdf2b7f599dd67c9a446c
George Bell <>
2019-08-22 21:48:36 -0400

    Add random image capability to all pages

--- src/components/RenderRouter/ContentItem.tsx
+++ src/components/RenderRouter/ContentItem.tsx
@@ -6,7 +6,6 @@ interface Props {
 interface State {
   data: any
 }
-//            <img src={this.state.data.image1Src} alt={this.state.data.image1Alt} style={{maskImage: "url("+this.state.data.image1Mask+")",width:this.state.data.image1Width}}/>
 
 export default class ContentItem extends React.Component<Props, State>  {
   constructor(props: Props) {
@@ -15,34 +14,26 @@ export default class ContentItem extends React.Component<Props, State>  {
       data: props.data
     }
   }
- 
-  render() {
-    if (this.state.data.style === "oneImage") return (
-      <div className="ContentItem oneImage" style={{ position: "static", paddingBottom: "5vw" }}>
-        <div style={{ position: "relative", zIndex: 100 }}>
-          <div style={{ padding:"10vw",position: "relative", width: "50vw", height: "40vw", left: "50vw", top: "0vw", backgroundColor: "#EFEFF0" }}>
-            <h1 style={{ fontWeight: "bold", fontSize: "3vw" }}>{this.state.data.header1}</h1>
-            <h2>{this.state.data.header2}</h2>
-            <div>{this.state.data.text1}</div>
-            <a href={this.state.data.link1Action}>{this.state.data.link1Text}</a>
-          </div>
-          <img src={this.state.data.image1Src} alt={this.state.data.image1Alt} style={{ position: "absolute", height: "35vw", left: "20vw", top: "4vw",boxShadow: "0px 20px 40px rgba(26, 26, 26, 0.16);"}} />
-        </div>
-      </div>
-    )
 
-    else if (this.state.data.style === "twoImage") return (
-      <div className="ContentItem twoImage" style={{ position: "static", paddingBottom: "5vw" }}>
-        <div style={{ position: "relative", zIndex: 100 }}>
-          <h1 style={{ fontWeight: "bold", fontSize: "3vw" }}>{this.state.data.header1}</h1>
-          <h2>{this.state.data.header2}</h2>
-          <div>{this.state.data.text1}</div>
-          <a href={this.state.data.link1Action}>{this.state.data.link1Text}</a>
-          <img src={this.state.data.image1Source} alt={this.state.data.image1Alt} style={{ width: this.state.data.image1Width }} />
-          <img src={this.state.data.image2Source} alt={this.state.data.image2Alt} style={{ width: this.state.data.image2Width }} />
+  render() {
+    if (this.state.data.style === "oneImage") {
+      var image1 = this.state.data.image1[Math.floor(Math.random() * this.state.data.image1.length)];
+
+      return (
+        <div className="ContentItem oneImage" style={{ position: "static", paddingBottom: "5vw" }}>
+          <div style={{ position: "relative", zIndex: 100 }}>
+            <div style={{ padding: "10vw", position: "relative", width: "50vw", height: "40vw", left: "50vw", top: "0vw", backgroundColor: "#EFEFF0" }}>
+              <h1 style={{ fontWeight: "bold", fontSize: "3vw" }}>{this.state.data.header1}</h1>
+              <h2>{this.state.data.header2}</h2>
+              <div>{this.state.data.text1}</div>
+              <a href={this.state.data.link1Action}>{this.state.data.link1Text}</a>
+            </div>
+            <img src={image1.src} alt={image1.alt} style={{ position: "absolute", height: "35vw", left: "20vw", top: "4vw", boxShadow: "0px 20px 40px rgba(26, 26, 26, 0.16);" }} />
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
+
 
     return (null)
   }
--- src/components/RenderRouter/HeaderItem.tsx
+++ src/components/RenderRouter/HeaderItem.tsx
@@ -16,6 +16,8 @@ export default class HeaderItem extends React.Component<Props, State> {
     }
     parallaxDetail=[200, 250];
     render() {
+        var image1 = this.state.data.image1[Math.floor(Math.random()*this.state.data.image1.length)];
+
         return (
             <div className="headerItem" style={{ position: "static", paddingBottom: "5vw" }}>
                 <div style={{ position: "relative", zIndex: 100 }}>
@@ -25,7 +27,7 @@ export default class HeaderItem extends React.Component<Props, State> {
                     <div style={{ fontSize: "1.5vw" }}>{this.state.data.text2}</div>
                     <Button>{this.state.data.button1Text}</Button>
                     <a href={this.state.data.link1Action}>{this.state.data.link1Text}</a>
-                    <img src={this.state.data.image1Src} alt={this.state.data.image1Alt} className="example-mask" style={{ position: "absolute", top: "5vw", left: "58vw", maskImage: 'url("' + this.state.data.image1Mask + '")', WebkitMaskImage: 'url("' + this.state.data.image1Mask + '")', width: this.state.data.image1Width }} />
+                    <img src={image1.src} alt={image1.alt} className="example-mask" style={{ position: "absolute", top: "5vw", left: "58vw", maskImage: 'url("' + this.state.data.image1Mask + '")', WebkitMaskImage: 'url("' + this.state.data.image1Mask + '")', width: this.state.data.image1Width }} />
                 </div>
             </div>
         )
