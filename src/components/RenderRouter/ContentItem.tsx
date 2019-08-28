5e2d095f5aff697bdd91c8f92cf3158f88d565a7
George Bell <>
2019-08-28 15:03:11 -0400

    Update formatting for oneImage module to mobile

--- src/components/RenderRouter/ContentItem.tsx
+++ src/components/RenderRouter/ContentItem.tsx
@@ -1,5 +1,7 @@
 
 import React from 'react';
+import "./ContentItem.scss"
+
 interface Props {
   data: any
 }
@@ -22,13 +24,13 @@ export default class ContentItem extends React.Component<Props, State>  {
       return (
         <div className="ContentItem oneImage" style={{ position: "static", paddingBottom: "5vw" }}>
           <div style={{ position: "relative", zIndex: 100 }}>
-            <div style={{ padding: "10vw", position: "relative", width: "50vw", height: "40vw", left: "50vw", top: "0vw", backgroundColor: "#EFEFF0" }}>
-              <h1 style={{ fontWeight: "bold", fontSize: "3vw", fontFamily:"Graphik Web" }}>{this.state.data.header1}</h1>
+            <div className="oneImageGreyBox" >
+              <h1 className="oneImageH1" >{this.state.data.header1}</h1>
               <h2>{this.state.data.header2}</h2>
               <div style={{fontSize: "1.5vw", fontFamily:"Graphik Web" }}>{this.state.data.text1}</div>
               <a style={{fontSize: "1.5vw",fontFamily:"Graphik Web"}} href={this.state.data.link1Action}>{this.state.data.link1Text}</a>
             </div>
-            <img src={image1.src} alt={image1.alt} style={{ position: "absolute", height: "35vw", width:"35vw", left: "20vw", objectFit:"cover", top: "2.5vw", boxShadow: "0px 20px 40px rgba(26, 26, 26, 0.16)" }} />
+            <img src={image1.src} alt={image1.alt} className="oneImageImage"  />
           </div>
         </div>
       )
