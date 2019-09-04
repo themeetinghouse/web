c351dbeb55093c239e23b55d71fa36f43699df76
George Bell <>
2019-09-04 15:58:02 -0400

    Load locations into map, create links to sitepages

--- src/components/RenderRouter/SundayMorningItem.tsx
+++ src/components/RenderRouter/SundayMorningItem.tsx
@@ -1,21 +1,24 @@
 
 import React from 'react';
-import {GoogleApiWrapper} from 'google-maps-react';
+import { GoogleApiWrapper } from 'google-maps-react';
 //import {ProviderProps} from 'google-maps-react';
-import { Marker }from 'google-maps-react';
-import {Map} from 'google-maps-react';
+import { Marker } from 'google-maps-react';
+import { Map } from 'google-maps-react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
 import "./SundayMorningItem.scss"
 
 
 
-interface Props  {
+interface Props extends RouteComponentProps  {
   content: any,
-  google:any
+  google: any
 
 }
 interface State {
   content: any,
-  selectedPlace: any
+  selectedPlace: any,
+  listData: any
 }
 
 export class ContentItem extends React.Component<Props, State>  {
@@ -24,8 +27,23 @@ export class ContentItem extends React.Component<Props, State>  {
     console.log(props)
     this.state = {
       content: props.content,
-      selectedPlace: null
+      selectedPlace: null,
+      listData: null
     }
+    this.navigate = this.navigate.bind(this);
+
+    fetch('./static/data/locations.json').then(function (response) {
+      return response.json();
+    })
+      .then((myJson) => {
+        this.setState({ listData: myJson });
+      })
+  }
+  navigate(to: string) {
+    this.props.history.push(to, "as")
+    const unblock = this.props.history.block('Are you sure you want to leave this page?');
+    unblock();
+
   }
 
   onMarkerClick() { }
@@ -33,17 +51,30 @@ export class ContentItem extends React.Component<Props, State>  {
 
   render() {
     return (
-      <div className="ContentItem oneImage" style={{ height:"100vw", position: "static", paddingBottom: "5vw" }}>
+      <div className="ContentItem oneImage" style={{ height: "100vw", position: "static", paddingBottom: "5vw" }}>
         <div style={{ position: "relative", left: "20vw", width: "80vw" }}>
           <div  >
             <h1  >{this.state.content.header1}</h1>
-            <Map google={this.props.google} zoom={14} className="sunday-morning-map" style={{left:"40vw",width:"40vw",height:"40vw"}}>
-
-              <Marker onClick={this.onMarkerClick}
-              />
-
-
-            </Map>
+            <div style={{ position: "absolute", left: "40vw", width: "40vw", height: "40vw" }}>
+              <Map google={this.props.google} zoom={6} initialCenter={{
+                lat: 44,
+                lng: -78.0
+              }}
+                style={{ left: "0vw", width: "40vw", height: "40vw" }}>
+                {this.state.listData != null ? this.state.listData.map((item: any) => {
+                  return (<Marker onClick={this.onMarkerClick}
+                    position={{ lat: item.location.latitude, lng: item.location.longitude }} />
+                  )
+                }) : null}
+              </Map>
+            </div>
+            <div style={{ position: "absolute", left: "0vw", width: "40vw", height: "40vw" }}>
+              {this.state.listData != null ? this.state.listData.map((item: any) => {
+                return (
+                  <div onClick={() => this.navigate(item.id)}>{item.name}</div>
+                )
+              }) : null}
+            </div>
           </div>
         </div>
       </div>
@@ -54,4 +85,4 @@ export class ContentItem extends React.Component<Props, State>  {
 
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyDXxLzyv5pYsIPl3XnVX5ONklXvs48zjn0')
-})(ContentItem)
+})(withRouter(ContentItem))
