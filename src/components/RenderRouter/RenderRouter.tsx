6751d718a2fc7c4a8d86b31d2de696e1ac6cf8f4
George Bell <>
2019-09-08 12:43:35 -0400

    Add hrefLang tag

--- src/components/RenderRouter/RenderRouter.tsx
+++ src/components/RenderRouter/RenderRouter.tsx
@@ -64,6 +64,7 @@ class RenderRouter extends React.Component<Props, State> {
             <title>{this.props.content.page.title}</title>
             <meta name="keywords" content={this.props.content.page.keywords}></meta>
             <meta name="description" content={this.props.content.page.description}></meta>
+            <link rel="alternate" href="https://beta.themeetinghouse.com" hrefLang="en-ca" />
           </Helmet>
           <HomeMenu pageConfig={this.props.content.page.pageConfig} ></HomeMenu>
           {this.renderItem()}
