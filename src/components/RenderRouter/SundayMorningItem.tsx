ab9da342866c3123b1278734c5270c176c473937
George Bell <>
2019-09-09 00:05:57 -0400

    Get Locations and times for sites

--- src/components/RenderRouter/SundayMorningItem.tsx
+++ src/components/RenderRouter/SundayMorningItem.tsx
@@ -10,7 +10,7 @@ import "./SundayMorningItem.scss"
 
 
 
-interface Props extends RouteComponentProps  {
+interface Props extends RouteComponentProps {
   content: any,
   google: any
 
@@ -18,7 +18,11 @@ interface Props extends RouteComponentProps  {
 interface State {
   content: any,
   selectedPlace: any,
-  listData: any
+  listData: any,
+  origin: any,
+  travelMode: string,
+  currentLatLng: any,
+  distances: any
 }
 
 export class ContentItem extends React.Component<Props, State>  {
@@ -28,16 +32,23 @@ export class ContentItem extends React.Component<Props, State>  {
     this.state = {
       content: props.content,
       selectedPlace: null,
-      listData: null
+      listData: null,
+      origin: ["43", "-79.8707"],
+      travelMode: "DRIVING",
+      currentLatLng: null,
+      distances: null
     }
     this.navigate = this.navigate.bind(this);
-
+    this.getGeoLocation()
     fetch('./static/data/locations.json').then(function (response) {
       return response.json();
     })
       .then((myJson) => {
         this.setState({ listData: myJson });
       })
+
+
+
   }
   navigate(to: string) {
     this.props.history.push(to, "as")
@@ -48,8 +59,41 @@ export class ContentItem extends React.Component<Props, State>  {
 
   onMarkerClick() { }
   onInfoWindowClose() { }
-
+  getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position.coords);
+          this.setState(prevState => ({
+            currentLatLng: {
+              ...prevState.currentLatLng,
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+          }))
+        }
+      )
+    } else {
+      console.log("error")
+    }
+  }
   render() {
+    if (this.state.listData != null && this.state.currentLatLng != null) {
+      if (this.state.distances == null) {
+        var service = new this.props.google.maps.DistanceMatrixService()
+        var destinations = this.state.listData.map((item: any) => {
+          return (new this.props.google.maps.LatLng(item.location.latitude, item.location.longitude))
+        })
+        var start = new this.props.google.maps.LatLng(this.state.currentLatLng.lat, this.state.currentLatLng.lng)
+
+        // service.getDistanceMatrix()
+        service.getDistanceMatrix({ origins: [start], destinations: destinations, travelMode: this.state.travelMode }, (res: any, status: any) => {
+          console.log(res)
+          if (status == "OK")
+            this.setState({ distances: res })
+        })
+      }
+    }
     return (
       <div className="ContentItem oneImage" style={{ height: "100vw", position: "static", paddingBottom: "5vw" }}>
         <div style={{ position: "relative", left: "20vw", width: "80vw" }}>
@@ -69,9 +113,9 @@ export class ContentItem extends React.Component<Props, State>  {
               </Map>
             </div>
             <div style={{ position: "absolute", left: "0vw", width: "40vw", height: "40vw" }}>
-              {this.state.listData != null ? this.state.listData.map((item: any) => {
+              {this.state.listData != null ? this.state.listData.map((item: any, index: any) => {
                 return (
-                  <div onClick={() => this.navigate(item.id)}>{item.name}</div>
+                  <div onClick={() => this.navigate(item.id)}>{item.name}{this.state.distances != null ? " - " + this.state.distances.rows[0].elements[index].distance.text + " " + this.state.distances.rows[0].elements[index].duration.text : null}</div>
                 )
               }) : null}
             </div>
