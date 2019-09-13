58a8e060805613dcaab6ca8faf7c0ec65a9affd1
George Bell <>
2019-09-12 21:01:50 -0400

    Distance Groups design and inital data

--- src/components/RenderRouter/HomeChurchItem.tsx
+++ src/components/RenderRouter/HomeChurchItem.tsx
@@ -11,7 +11,7 @@ import { Button } from 'react-bootstrap';
 
 
 
-interface Props extends RouteComponentProps  {
+interface Props extends RouteComponentProps {
   content: any,
   google: any
 
@@ -33,12 +33,12 @@ export class ContentItem extends React.Component<Props, State>  {
     }
     this.navigate = this.navigate.bind(this);
 
-  /*  fetch('./static/data/locations.json').then(function (response) {
+    fetch('./static/data/distance-groups.json').then(function (response) {
       return response.json();
     })
       .then((myJson) => {
         this.setState({ listData: myJson });
-      })*/
+      })
   }
   navigate(to: string) {
     this.props.history.push(to, "as")
@@ -51,16 +51,30 @@ export class ContentItem extends React.Component<Props, State>  {
   onInfoWindowClose() { }
 
   render() {
+    if (this.state.content.class == "home-church") {
+      var inititalCenter: any = {
+        lat: 44,
+        lng: -78.0
+
+      }
+      var initalZoom = 6
+    }
+    else {
+      var inititalCenter: any = {
+        lat: 0,
+        lng: -0
+      }
+      var initalZoom = 1
+    }
+
     return (
+
       <div className="ContentItem oneImage" style={{ height: "100vw", position: "static", paddingBottom: "5vw" }}>
         <div style={{ position: "relative", left: "20vw", width: "80vw" }}>
           <div  >
             <h1  >{this.state.content.header1}</h1>
             <div style={{ position: "absolute", left: "40vw", width: "40vw", height: "40vw" }}>
-              <Map google={this.props.google} zoom={6} initialCenter={{
-                lat: 44,
-                lng: -78.0
-              }}
+              <Map google={this.props.google} zoom={initalZoom} initialCenter={inititalCenter}
                 style={{ left: "0vw", width: "40vw", height: "40vw" }}>
                 {this.state.listData != null ? this.state.listData.map((item: any) => {
                   return (<Marker onClick={this.onMarkerClick}
@@ -71,12 +85,25 @@ export class ContentItem extends React.Component<Props, State>  {
             </div>
             <div style={{ position: "absolute", left: "0vw", width: "40vw", height: "40vw" }}>
               {this.state.listData != null ? this.state.listData.map((item: any) => {
-                return (
-                  <div >{item.name}
-                  <Button onClick={() => this.navigate(item.id)}>Site Page</Button>
-                  </div>
-                )
+                if (this.state.content.class == "home-church") {
+                  return (
+                    <div >{item.name}
+                      <Button onClick={() => this.navigate(item.id)}>Site Page</Button>
+                    </div>
+                  )
+                }
+                else {
+                  return (
+                    <div ><h3>{item.name}</h3>
+                      <div>{item.dayofweek}/{item.frequency}</div>
+                      <div>Leaders: <a href={"mailto:" + item.l1Email}>{item.l1FirstName} {item.l1LastName} ({item.l1Email})</a></div>
+                      <div ><a href={item.facebookLink} style={{ whiteSpace: "nowrap", color: "#1A1A1A" }}><img style={{ marginRight: "0.5vw", marginTop: "0.5vw", marginBottom: "0.5vw" }} src="/static/svg/Facebook.svg" alt="Facebook Logo" />{item.facebook}</a> </div>
+
+                    </div>
+                  )
+                }
               }) : null}
+
             </div>
           </div>
         </div>
