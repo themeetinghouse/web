623a81b9968afdcda008c5cdc78a8796959b081f
George Bell <georgebell@Georges-MacBook-Pro.local>
2020-03-12 13:17:27 -0400

    sunnday live

--- src/components/RenderRouter/VideoPlayerLive.tsx
+++ src/components/RenderRouter/VideoPlayerLive.tsx
@@ -1,6 +1,9 @@
 
 import React from 'react';
 import "./VideoPlayerLive.scss"
+import * as queries from '../../graphql/queries';
+import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api/lib/types';
+import { API } from 'aws-amplify';
 
 interface Props {
   content: any,
@@ -9,15 +12,76 @@ interface Props {
 interface State {
   data: any,
   content: any,
+  listData: any,
+  kidData: any
 }
 export default class VideoPlayer extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
+      listData: null,
+      kidData: null,
       content: props.content,
       data: props.data
     }
     console.log({ "VideoPlayer": props.data })
+    const getVideoByVideoType: any = API.graphql({
+      query: queries.getVideoByVideoType,
+      variables: { sortDirection: "DESC", limit: 2, videoTypes: 'adult-sunday', publishedDate: { lt: "a" } },
+      authMode: GRAPHQL_AUTH_MODE.API_KEY
+    });
+    getVideoByVideoType.then((json: any) => {
+      console.log("Success queries.getVideoByVideoType: " + json);
+      console.log(json)
+      this.setState({
+        listData: json.data.getVideoByVideoType.items
+      })
+    }).catch((e: any) => { console.log(e) })
+
+    const getVideoByVideoType1: any = API.graphql({
+      query: queries.getVideoByVideoType,
+      variables: { sortDirection: "DESC", limit: 1, videoTypes: "ky-kids", publishedDate: { lt: "a" } },
+      authMode: GRAPHQL_AUTH_MODE.API_KEY
+    });
+    const getVideoByVideoType2: any = API.graphql({
+      query: queries.getVideoByVideoType,
+      variables: { sortDirection: "DESC", limit: 1, videoTypes: "ky-jrhigh", publishedDate: { lt: "a" } },
+      authMode: GRAPHQL_AUTH_MODE.API_KEY
+    });
+    const getVideoByVideoType3: any = API.graphql({
+      query: queries.getVideoByVideoType,
+      variables: { sortDirection: "DESC", limit: 1, videoTypes: "ky-youth", publishedDate: { lt: "a" } },
+      authMode: GRAPHQL_AUTH_MODE.API_KEY
+    });
+    const getVideoByVideoType4: any = API.graphql({
+      query: queries.getVideoByVideoType,
+      variables: { sortDirection: "DESC", limit: 1, videoTypes: "ky-srhigh", publishedDate: { lt: "a" } },
+      authMode: GRAPHQL_AUTH_MODE.API_KEY
+    });
+    getVideoByVideoType1.then((json1: any) => {
+      console.log({ "Success queries.getVideoByVideoType: ": json1 });
+      this.setState({
+        kidData: json1.data.getVideoByVideoType.items
+      })
+      getVideoByVideoType2.then((json2: any) => {
+        console.log({ "Success queries.getVideoByVideoType: ": json2 });
+        this.setState({
+          kidData: this.state.kidData.concat(json2.data.getVideoByVideoType.items)
+        })
+        getVideoByVideoType3.then((json3: any) => {
+          console.log({ "Success queries.getVideoByVideoType: ": json3 });
+          this.setState({
+            kidData: this.state.kidData.concat(json3.data.getVideoByVideoType.items)
+          })
+          getVideoByVideoType4.then((json4: any) => {
+            console.log({ "Success queries.getVideoByVideoType: ": json4 });
+            this.setState({
+              kidData: this.state.kidData.concat(json4.data.getVideoByVideoType.items)
+            })
+          }).catch((e: any) => { console.log(e) })
+        }).catch((e: any) => { console.log(e) })
+      }).catch((e: any) => { console.log(e) })
+    }).catch((e: any) => { console.log(e) })
 
   }
   render() {
@@ -26,27 +90,58 @@ export default class VideoPlayer extends React.Component<Props, State> {
         <div className="LiveVideoPlayerEpisodeTitle">Today's Teaching Notes</div>
         <div className="LiveVideoPlayerExtra">
           <div className="LiveVideoPlayerSeriesNotes"><a href="http://media.themeetinghouse.com/podcast/handouts/Oakville.pdf">Oakville Location</a></div>
-          <br/>
+          <br />
           <div className="LiveVideoPlayerSeriesNotes"><a href="http://media.themeetinghouse.com/podcast/handouts/Regional.pdf">Regional Locations</a></div>
           <div className="LiveVideoPlayerClear"></div>
         </div>
         <div className="LiveVideoPlayerExtra">
           <div className="LiveVideoPlayerSeriesNotes">Click on one of the locations above to access today's teaching notes. Notes are updated on Sunday and will be available again on Monday on the teaching page.</div>
-       </div> 
+        </div>
         {/* <div className="LiveVideoPlayerExtra">
           <div className="LiveVideoPlayerSeriesNotes"><a href="TODO">KidMax Sign-In</a></div>
           <div className="LiveVideoPlayerClear"></div>
         </div>
-        */}   <br/>
-        <br/>
-       
-        <div className="LiveVideoPlayerEpisodeTitle">Oakville Live Feed</div>
-        <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/test?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-        <div className="LiveVideoPlayerEpisodeTitle">Regional Recording</div>
-        <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/test?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-        <div className="LiveVideoPlayerEpisodeTitle">KidMax Recording</div>
-        <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/test?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-       </div>
+        */}
+        {this.state.content.showLiveVideos ? (
+          <div>
+            <br />
+            <br />
+
+            <div className="LiveVideoPlayerEpisodeTitle">Oakville Live Feed</div>
+            <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/" + this.state.content.liveYoutubeId + "?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+            <div className="LiveVideoPlayerEpisodeTitle">Regional Recording</div>
+            {this.state.listData ?
+              <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/" + this.state.listData[0].id + "?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+              : null
+            }
+            <div className="LiveVideoPlayerEpisodeTitle">KidMax Recording</div>
+            {this.state.kidData ?
+              <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/" + this.state.kidData[0].id + "?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+              : null
+            }
+            <div className="LiveVideoPlayerEpisodeTitle">JrHigh Recording</div>
+            {this.state.kidData ?
+              this.state.kidData[1] ?
+                <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/" + this.state.kidData[1].id + "?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                : null : null
+            }
+            <div className="LiveVideoPlayerEpisodeTitle">Youth Recording</div>
+            {this.state.kidData ?
+              this.state.kidData[2] ?
+                <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/" + this.state.kidData[2].id + "?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                : null : null
+            }
+            <div className="LiveVideoPlayerEpisodeTitle">Sr High Recording</div>
+            {this.state.kidData ?
+              this.state.kidData[3] ?
+                <iframe title="Youtube Player" className="LiveVideoPlayerIframe" allowFullScreen src={"https://www.youtube.com/embed/" + this.state.kidData[3].id + "?color=white&autoplay=0&cc_load_policy=1&showTitle=0&controls=1&modestbranding=1&rel=0"} frameBorder="0" allow="speakers; fullscreen; accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                : null : null
+            }
+          </div>
+          ) : null}
+      </div>
+
+
     )
 
   }
