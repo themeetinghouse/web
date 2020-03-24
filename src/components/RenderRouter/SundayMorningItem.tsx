e436b031af85d5dbb05b8eb75194f612a357e18f
Jon Esparaz <jon.esparaz@gmail.com>
2020-03-24 00:29:58 -0400

    desktop ui done

--- src/components/RenderRouter/SundayMorningItem.tsx
+++ src/components/RenderRouter/SundayMorningItem.tsx
@@ -303,7 +303,7 @@ export class ContentItem extends React.Component<Props, State>  {
                             <img className="AddToCalendarIcon" src="/static/Calendar.png" alt="Calendar Icon" />
                             <AddToCalendar buttonLabel="Add to Calendar" event={this.getCalendarEventForLocation(item)} ></AddToCalendar>
                           </div>
-                          <a href={"mailto:"+item.pastorEmail}><button className="emailButton"><img className="calendarImage"  src="/static/svg/Contact.svg" alt="Contact Icon" />Contact the Pastor</button></a>
+                          <a className="emailText" href={"mailto:"+item.pastorEmail}><button className="emailButton"><img className="emailImage"  src="/static/svg/Contact.svg" alt="Contact Icon" /></button>Contact the Pastor</a>
                         </div>
                       </div>
 
