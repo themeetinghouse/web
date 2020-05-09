11a4c86793b3071e57cd333571125433bfb2a977
George Bell <>
2020-05-09 18:29:02 -0400

    Save environment to sentry

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,18 @@ import * as Sentry from '@sentry/browser'
 import App from './App'
 import { createBrowserHistory, History } from "history";
 import { CookiesProvider } from 'react-cookie';
+var env = "unknown"
+if (window.location.hostname === "localhost")
+  env = "dev"
+else if (window.location.hostname.includes("beta"))
+  env = "beta"
+else
+  env = "prod"
 
-Sentry.init({dsn: "https://38e8f0bc706a4d968e1ff3ebf638a090@o390245.ingest.sentry.io/5231570"});
+Sentry.init({
+  dsn: "https://38e8f0bc706a4d968e1ff3ebf638a090@o390245.ingest.sentry.io/5231570",
+  environment: env
+});
 
 var history: History<any> = createBrowserHistory()
 
