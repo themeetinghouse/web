a45333700e13eea4c55dcab5b75eb63d0a6217b3
Jon Esparaz <jon.esparaz@gmail.com>
2020-05-11 19:22:44 -0400

    badges added, near-final styling

--- src/components/RenderRouter/PodcastItem.tsx
+++ src/components/RenderRouter/PodcastItem.tsx
@@ -1,6 +1,8 @@
 
 import React from 'react';
 import "./PodcastItem.scss"
+import Dropdown from 'react-bootstrap/Dropdown';
+import Fade from 'react-bootstrap/Fade';
 
 interface Props {
   content: any,
@@ -24,14 +26,24 @@ export default class PodcastItem extends React.Component<Props, State> {
   renderPodcast(item: any) {
     return (
       <div className="PodcastItemDiv1">
-        <img src={item.icon.src} alt={item.icon.alt} style={{width: 64, height: 64}}></img>
+        <img className="PodcastIcon" src={item.icon.src} alt={item.icon.alt} style={{width: 64, height: 64}}></img>
         <div className="PodcastContentContainer">
           <div className="PodcastTitle">{item.title}</div>
           <div className="PodcastText">{item.description}</div>
 
           <div className="PodcastLinksContainer">
-            <a className="PodcastLink"><img className="PodcastIcon" src="/static/svg/iTunes-white.svg" alt="" style={{width: 16, height: 16}}></img>iTunes</a>
-            <a className="PodcastLink"><img className="PodcastIcon" src="/static/svg/Subscribe-white.svg" alt="" style={{width: 16, height: 16}}></img>Subscribe</a>
+            <a className="ApplePodcast" href={item.apple} rel="noopener noreferrer" target="_blank"><img src="/static/US_UK_Apple_Podcasts_Listen_Badge_RGB.svg" alt="Apple Podcasts" style={{height: 30}}></img></a>
+         
+            <Dropdown className="PodcastDropdown">
+              <Dropdown.Toggle id="podcast-toggle"><img className="SubIcon" src="/static/svg/Subscribe-white.svg" alt=""/>More ways to listen</Dropdown.Toggle>
+                <Fade timeout={1000}>
+                  <Dropdown.Menu className="PodcastMenu">
+                    <a href={item.spotify} rel="noopener noreferrer" target="_blank"><img src="/static/spotify-podcast-badge-wht-grn-165x40.svg" alt="Spotify" style={{height: 30, marginBottom: 10}}></img></a>
+                    <a href={item.google} rel="noopener noreferrer" target="_blank"><img src="/static/EN_Google_Podcasts_Badge.svg" alt="Google Podcasts" style={{height: 30, marginBottom: 10}}></img></a>
+                    <a href={item.podbean} rel="noopener noreferrer" target="_blank"><img src="//d8g345wuhgd7e.cloudfront.net/site/images/badges/w600.png" alt="Podbean" style={{height: 30}}></img></a>
+                  </Dropdown.Menu>
+                </Fade>
+            </Dropdown>
           </div>
 
         </div>
