dbb208f02b00fae59a0a2a5bc3aec5e20a091931
Jon Esparaz <jon.esparaz@gmail.com>
2020-05-15 21:43:35 -0400

    clean up code. add comments.

--- src/pages/admin/create-blog.tsx
+++ src/pages/admin/create-blog.tsx
@@ -109,21 +109,10 @@ class IndexApp extends React.Component<Props, State> {
       })
 
     this.listSeries(null)
-    this.handleChangeTitle = this.handleChangeTitle.bind(this);
-    this.handleChangeAuthor = this.handleChangeAuthor.bind(this);
-    this.handleChangeDesc = this.handleChangeDesc.bind(this);
-    this.handleChangeTag = this.handleChangeTag.bind(this);
-    this.handleVideoSeries = this.handleVideoSeries.bind(this);
-    this.showPreview = this.showPreview.bind(this);
-    this.confirmTag = this.confirmTag.bind(this);
-    this.clearTags = this.clearTags.bind(this);
-    this.confirmBlogSeries = this.confirmBlogSeries.bind(this);
-    this.clearBlogSeries = this.clearBlogSeries.bind(this);
-    this.confirmVideoSeries = this.confirmVideoSeries.bind(this);
-    this.clearVideoSeries = this.clearVideoSeries.bind(this);
-    this.moreOptions = this.moreOptions.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
   }
+
+  //QUERY FUNCTIONS
 
   listSeries(nextToken: any) {
     var listSeries:any = API.graphql({
@@ -143,26 +132,7 @@ class IndexApp extends React.Component<Props, State> {
     }).catch((e: any) => { console.log(e) })
   }
 
-  onChange = (editorState: any) => this.setState({ editorState });
-
-  onImageChange(e: any) {
-    const file = e.target.files[0];
-    const filename = file.name;
-    Storage.put(filename, file, {
-        contentType: 'image/*'
-    })
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
-
-    Storage.get(filename)
-    .then(result => this.setState({imgURL: result}))
-    .catch(err => console.log(err));
-  }
-
-  getMarkup() {
-    const markup = draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()));
-    return markup
-  }
+  //HANDLERS
 
   handleSave() {
     //if this.state.editMode === true
@@ -187,73 +157,33 @@ class IndexApp extends React.Component<Props, State> {
     //verbose warning that you need to re-publish
   }
 
-  renderEditBlog() {
-    return <Modal isOpen={this.state.showEditModal}>
-        <div>Test</div>
-        <button onClick={() => { this.setState({ showEditModal: false }) }}>DONE</button>
-      </Modal>
-  }
+  //add new blog series function
 
   handleExit() {
     //provide warnings
   }
 
-  handleVideoSeries(event: any) {
-    this.setState({ currentVideoSeries: event.target.value });
+  //MISC FUNCTIONS + HELPERS
+
+  onChange = (editorState: any) => this.setState({ editorState });
+
+  onImageChange(e: any) {
+    const file = e.target.files[0];
+    const filename = file.name;
+    Storage.put(filename, file, {
+        contentType: 'image/*'
+    })
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+
+    Storage.get(filename)
+    .then(result => this.setState({imgURL: result}))
+    .catch(err => console.log(err));
   }
 
-  handleChangeTitle(event: any) {
-    this.setState({ title: event.target.value });
-  }
-
-  handleChangeAuthor(event: any) {
-    this.setState({ author: event.target.value });
-  }
-
-  handleChangeDesc(event: any) {
-    this.setState({ desc: event.target.value });
-  }
-
-  handleChangeTag(event: any) {
-    this.setState({ currentTag: event.target.value });
-  }
-
-  showPreview() {
-    this.setState({ showPreview: !this.state.showPreview })
-  }
-
-  confirmTag() {
-    this.setState({ 
-      selectedTags: this.state.selectedTags.concat(this.state.currentTag), 
-      currentTag: ''
-    });
-  }
-
-  clearTags() {
-    this.setState({ selectedTags: [] });
-  }
-
-  confirmVideoSeries() {
-    this.setState({ selectedVideoSeries: this.state.selectedVideoSeries.concat(this.state.currentVideoSeries) });
-  }
-
-  clearVideoSeries() {
-    this.setState({ selectedVideoSeries: [] });
-  }
-
-  confirmBlogSeries() {
-    this.setState({ selectedVideoSeries: this.state.selectedVideoSeries.concat('') });
-  }
-
-  clearBlogSeries() {
-    this.setState({ selectedBlogSeries: [] });
-  }
-
-  //add new blog series function
-
-  moreOptions() {
-    this.setState({ moreOptions: !this.state.moreOptions })
-    console.log(this.state.videoSeriesList);
+  getMarkup() {
+    const markup = draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()));
+    return markup
   }
 
   interval: any;
@@ -269,97 +199,125 @@ class IndexApp extends React.Component<Props, State> {
     this.setState({ saveReminder: !this.state.saveReminder })
   }
 
-  //image only width control.
+  //RENDER FUNCTIONS
+
+  renderEditBlog() {
+    return <Modal isOpen={this.state.showEditModal}>
+        <div>Test</div>
+        <button onClick={() => { this.setState({ showEditModal: false }) }}>DONE</button>
+      </Modal>
+  }
+
+  renderMoreOptions() {
+    return (
+      <div>
+        <label>
+          Add tags:
+          <input type="text" value={this.state.currentTag} onChange={(event:any) => this.setState({ currentTag: event.target.value})} />
+        </label>
+        <button className="tags-button" onClick={()=>this.setState({selectedTags: this.state.selectedTags.concat(this.state.currentTag), currentTag: ''})}>Confirm Tag</button>
+        <button className="tags-button" style={{background: "red"}} onClick={() => this.setState({ selectedTags: [] })}>Clear Selection</button>
+        <div>Current tags: {this.state.selectedTags.map((item: any) => item + ", ")}</div>
+          <br/>
+
+        <b>Add to Video Series</b>
+        <button className="tags-button" onClick={() => this.setState({ selectedVideoSeries: this.state.selectedVideoSeries.concat(this.state.currentVideoSeries)})}>Add</button>
+        <button className="tags-button" style={{background: "red"}} onClick={() => this.setState({ selectedVideoSeries: [] })}>Clear Selection</button>
+        <select onChange={(event:any) => this.setState({ currentVideoSeries: event.target.value})}>
+          <option key="null" value="null">None Selected</option>
+          {this.state.videoSeriesList.map((item: any) => {return <option key={item.id} value={item.id}>{item.id}</option>})}
+        </select>
+        <div>Current video series: {this.state.selectedVideoSeries.map((item: any) => item + ", ")}</div>
+          <br/>
+
+        <b>Add to Blog Series</b>
+        <button className="tags-button" onClick={()=>this.setState({ selectedVideoSeries: this.state.selectedVideoSeries.concat('')})}>Add</button>
+        <button className="tags-button" style={{background: "red"}} onClick={()=>this.setState({ selectedBlogSeries: [] })}>Clear Selection</button>
+        <select>
+          <option>test</option>
+        </select>
+        <div>Current blog series: {this.state.selectedBlogSeries.map((item: any) => item + ", ")}</div>
+      </div>
+    )
+  }
+
+  renderTextInput() {
+    return (
+      <div className="editor-container">
+        <label>
+          Title:
+          <input className="small-input" type="text" value={this.state.title} onChange={(event:any)=> this.setState({ title: event.target.value})} />
+        </label>
+
+        <label>
+          Author:
+          <input className="small-input" type="text" value={this.state.author} onChange={(event:any)=> this.setState({ author: event.target.value})} />
+        </label>
+
+        <label style={this.state.desc.length >= 180 ? {color: "red"} : {color: "black"}}>
+          Description ({this.state.desc.length + " of 220 characters"}):
+          <textarea className="big-input" maxLength={220} value={this.state.desc} onChange={(event:any)=> this.setState({ desc: event.target.value})} />
+        </label>
+
+        <Editor
+          editorState={this.state.editorState}
+          onEditorStateChange={this.onChange}
+          spellCheck={true}
+          toolbar={{
+            options: ['inline', 'blockType', 'fontSize', 'list', 'link', 'emoji', 'image', 'history'],
+            inline: {
+              options: ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript'],
+            },
+            list: {
+              options: ['unordered', 'ordered']
+            },
+            image: {
+              alignmentEnabled: false,
+              defaultSize: {
+                height: 'auto',
+                width: '50vw'
+              }
+            }
+          }}
+        />
+        <label>
+          Upload an image:
+          <br/>
+          <input
+            type="file" accept='image/*'
+            onChange={(evt) => this.onImageChange(evt)}
+          />
+        </label>
+        {/* add click to copy*/}
+        <div className="imageURL-output">{this.state.imgURL ? "Please use this URL: " + this.state.imgURL : null}</div>                   
+        {this.state.moreOptions ? this.renderMoreOptions() : null}
+      </div>
+    )
+  }
+
+  renderToolbar() {
+    return (
+      <div className="toolbar-button-container">
+        <button className="toolbar-button" onClick={this.handleSave}>SAVE</button><br/>
+        <button className="toolbar-button" onClick={this.handlePublish}>PUBLISH</button><br/>
+        <button className="toolbar-button" onClick={this.handleEdit}>Edit an existing post</button><br/>
+        <button className="toolbar-button" onClick={()=>this.setState({ moreOptions: !this.state.moreOptions })}>Add tags + series</button><br/>
+        <button className="toolbar-button" onClick={()=>this.setState({ showPreview: !this.state.showPreview })}>Preview your work</button>{this.state.showPreview ? <div style={{width: 150}}>Scroll to bottom of page for preview</div> : null}<br/>
+        {this.state.saveReminder ? <div style={{color: "red", fontWeight: 'bold', width: 180}}>Save Your Work!</div>: null}
+      </div>
+    )
+  }
 
   render() {
     return (
       <div className="blog-container">
         <AdminMenu></AdminMenu>
-          {this.renderEditBlog()}
-          <div className="toolbar-button-container">
-            <button className="toolbar-button" onClick={this.handleSave}>SAVE</button><br/>
-            <button className="toolbar-button" onClick={this.handlePublish}>PUBLISH</button><br/>
-            <button className="toolbar-button" onClick={this.handleEdit}>Edit an existing post</button><br/>
-            <button className="toolbar-button" onClick={this.moreOptions}>Add tags + series</button><br/>
-            <button className="toolbar-button" onClick={this.showPreview}>Preview your work</button>{this.state.showPreview ? <div style={{width: 150}}>Scroll to bottom of page for preview</div> : null}<br/>
-            {this.state.saveReminder ? <div style={{color: "red", fontWeight: 'bold', width: 180}}>Save Your Work!</div>: null}
-          </div>
-        <div className="editor-container">
-          <label>
-            Title:
-            <input className="small-input" type="text" value={this.state.title} onChange={this.handleChangeTitle} />
-          </label>
-
-          <label>
-            Author:
-            <input className="small-input" type="text" value={this.state.author} onChange={this.handleChangeAuthor} />
-          </label>
-
-          <label>
-            Description ({this.state.desc.length + " of 280 characters"}):
-            <textarea className="big-input" maxLength={280} value={this.state.desc} onChange={this.handleChangeDesc} />
-          </label>
-
-          <Editor
-            editorState={this.state.editorState}
-            onEditorStateChange={this.onChange}
-            spellCheck={true}
-            toolbar={{
-              options: ['inline', 'blockType', 'fontSize', 'list', 'link', 'emoji', 'image', 'history'],
-              inline: {
-                options: ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript'],
-              },
-              list: {
-                options: ['unordered', 'ordered']
-              },
-              image: {
-                alignment: false
-              }
-            }}
-          />
-          <label>
-            Upload an image:
-            <br/>
-            <input
-              type="file" accept='image/*'
-              onChange={(evt) => this.onImageChange(evt)}
-            />
-          </label>
-          {/* add click to copy*/}
-          <div className="imageURL-output">{this.state.imgURL ? "Please use this URL: " + this.state.imgURL : null}</div>                   
-          
-          {this.state.moreOptions ?
-          <div>
-            <label>
-              Add tags:
-              <input type="text" value={this.state.currentTag} onChange={this.handleChangeTag} />
-            </label>
-            <button className="tags-button" onClick={this.confirmTag}>Confirm Tag</button>
-            <button className="tags-button" style={{background: "red"}} onClick={this.clearTags}>Clear Selection</button>
-            <div>Current tags: {this.state.selectedTags.map((item: any) => item + ", ")}</div>
-              <br/>
-
-            <b>Add to Video Series</b>
-            <button className="tags-button" onClick={this.confirmVideoSeries}>Add</button>
-            <button className="tags-button" style={{background: "red"}} onClick={this.clearVideoSeries}>Clear Selection</button>
-            <select onChange={this.handleVideoSeries}>
-              <option key="null" value="null">None Selected</option>
-              {this.state.videoSeriesList.map((item: any) => {return <option key={item.id} value={item.id}>{item.id}</option>})}
-            </select>
-            <div>Current video series: {this.state.selectedVideoSeries.map((item: any) => item + ", ")}</div>
-              <br/>
-
-            <b>Add to Blog Series</b>
-            <button className="tags-button" onClick={this.confirmBlogSeries}>Add</button>
-            <button className="tags-button" style={{background: "red"}} onClick={this.clearBlogSeries}>Clear Selection</button>
-            <select>
-              <option>test</option>
-            </select>
-            <div>Current blog series: {this.state.selectedBlogSeries.map((item: any) => item + ", ")}</div>
-          </div>
-          : null}
+        {this.renderEditBlog()}
+        {this.renderToolbar()}
+        {this.renderTextInput()}
+        <div className="preview">
+          {this.state.showPreview ? <BlogPreview data={this.state} content={this.state.content}></BlogPreview> : null}
         </div>
-        <div className="preview">{this.state.showPreview ? <BlogPreview data={this.state} content={this.state.content}></BlogPreview> : null}</div>
       </div>
     );
   }
