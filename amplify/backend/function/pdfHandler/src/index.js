740671f77550ef0270d778bdb56cbc6cff015ff9
Jon Esparaz <jon.esparaz@gmail.com>
2020-05-29 17:06:28 -0400

    re-init func with storage access

---
+++ amplify/backend/function/pdfHandler/src/index.js
@@ -0,0 +1,18 @@
+/* Amplify Params - DO NOT EDIT
+	API_THEMEETINGHOUSE_GRAPHQLAPIENDPOINTOUTPUT
+	API_THEMEETINGHOUSE_GRAPHQLAPIIDOUTPUT
+	API_THEMEETINGHOUSE_NOTESTABLE_ARN
+	API_THEMEETINGHOUSE_NOTESTABLE_NAME
+	ENV
+	REGION
+	STORAGE_TMHUSERCONTENT_BUCKETNAME
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
