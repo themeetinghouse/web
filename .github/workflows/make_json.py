74f19b47dfd8999bfad8f7dbd53a48d20cc0bd90
Jon Esparaz <jon.esparaz@gmail.com>
2020-06-07 16:27:18 -0400

    patch job generate

--- .github/workflows/make_json.py
+++ .github/workflows/make_json.py
@@ -1,9 +1,12 @@
 import json
 import shutil
+import os
 
-with open('../../public/static/data/redirect.json') as file:
+rootdir = os.getcwd()
+
+with open(rootdir + '/public/static/data/redirect.json') as file:
     data = json.load(file)
 
 for i in data:
-    filepath = '../../public/static/redirect/' + i['id'] + '.json'
-    shutil.copyfile('redirect_template.json', filepath)
+    filepath = rootdir + '/public/static/redirect/' + i['id'] + '.json'
+    shutil.copyfile('./.github/workflows/redirect_template.json', filepath)
