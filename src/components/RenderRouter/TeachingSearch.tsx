a3332aeb86a9f4b623c037b4c2ed718b794b5dfc
Jon Esparaz <jon.esparaz@gmail.com>
2020-06-28 22:09:59 -0400

    teaching search styling (wip)

--- src/components/RenderRouter/TeachingSearch.tsx
+++ src/components/RenderRouter/TeachingSearch.tsx
@@ -8,9 +8,8 @@ import * as queries from '../../graphql/queries';
 
 Amplify.configure(awsmobile);
 
-interface Props extends RouteComponentProps{
+interface Props extends RouteComponentProps {
   content: any
-
 }
 interface State {
   content: any,
@@ -18,7 +17,7 @@ interface State {
   currentInput: string
 }
 
- class ContentItem extends React.Component<Props, State>  {
+class ContentItem extends React.Component<Props, State>  {
   constructor(props: Props) {
     super(props);
     this.state = {
@@ -31,23 +30,17 @@ interface State {
   doSearch(e: any, videoType: any) {
     const targetValue = e.target.value
     this.setState({ currentInput: targetValue })
-    this.search(targetValue, videoType, null)
+    this.search(targetValue, videoType)
   }
 
-  search(e: any, videoType: any, nextId: any) {
-
-    console.log(e)
-    const fuzzySearchVideos:any = API.graphql(graphqlOperation(queries.fuzzySearchVideos, { videoTypes: videoType, filter: e, limit: 10, nextToken: nextId }));
-    fuzzySearchVideos.then((json: any) => {
+  async search(e: any, videoType: any) {
+    try {
+      const json: any = await API.graphql(graphqlOperation(queries.fuzzySearchVideos, { videoType: videoType, filter: e, limit: 10, nextToken: null }));
       console.log(json)
-      if (nextId == null)
-        this.setState({ searchResults: json.data.fuzzySearchVideos.items })
-      else
-        this.setState({ searchResults: this.state.searchResults.concat(json.data.fuzzySearchVideos.items) })
-
-    }).catch((e: any) => {
-      console.log(e)
-    })
+      this.setState({ searchResults: json.data.fuzzySearchVideos.items })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   formatVideoDuration(length: string): string {
@@ -57,57 +50,50 @@ interface State {
       return length + 'm'
     }
     while (duration > 59) {
-      duration-=60
-      hours+=1
+      duration -= 60
+      hours += 1
     }
     return `${hours.toString()}h ${duration.toString()}m`
   }
 
   openVideo(item: any) {
-    console.log(item)
-    console.log("/videos/"+item.series.id+"/"+item.id)
-    this.navigateTo("/videos/"+item.series.id+"/"+item.id)
-//    this.navigateTo("/videos/"+item.series+"/"+item.episodeId)
-  }
-
-  navigateTo(location:any) {
-    this.props.history.push(location, "as")
+    this.props.history.push("/videos/" + item.series.id + "/" + item.id, "as")
     const unblock = this.props.history.block('Are you sure you want to leave this page?');
     unblock();
 
   }
 
-
-
-  // need an acutal fn for trending searches (don't use content from json)
   render() {
-    const focusInputField = (input:any) => {
+    const focusInputField = (input: any) => {
       if (input) {
-        setTimeout(() => {input.focus()}, 100);
+        setTimeout(() => { input.focus() }, 100);
       }
     };
-    return (<div className="SearchItem">
+    return (<div className="TeachingSearchItem">
       <div className="Header1">{this.props.content.header1}</div>
-      <img src="/static/svg/Search.svg" alt="Search" style={{paddingRight: "30px", paddingBottom:"6px"}}></img>
-      <input className="SearchItemInput" ref={focusInputField} value={this.state.currentInput} onChange={(e: any) => { this.doSearch(e, this.props.content.subclass) }} placeholder={this.props.content.text1}></input>
-      {this.state.searchResults.length ? <img onClick={()=>this.setState({ searchResults: [], currentInput: '' })} src="/static/svg/Close-Cancel.svg" alt="" className="CloseTeachingSearch"></img> : null}
-      <div className="SearchItemDiv">
-        <div className="TrendingSearches"/>
-          {this.state.searchResults.length ? <div>{this.state.searchResults.length} Videos</div> : null}
-          {this.state.searchResults.length ? this.state.searchResults.map((item: any) => {
-            if (item.episodeTitle !== null)
-              return (
-                <div key={item.id} onClick={() => { this.openVideo(item) }} className="SearchResultItem">
-                  <div className="Content">
-                    <div className="Title">{item.episodeTitle} - {item.seriesTitle}</div>
-                    {item.publishedDate ? <div>{item.publishedDate}</div> : null}
-                    {item.length ? <div>{this.formatVideoDuration(item.length)}</div> : null}
-                    <div className="Description">{item.description}</div>
+      <img src="/static/svg/Search.svg" alt="Search" style={{ paddingRight: "30px", paddingBottom: "6px" }}></img>
+      <input className="TeachingSearchItemInput" ref={focusInputField} value={this.state.currentInput} onChange={(e: any) => { this.doSearch(e, this.props.content.subclass) }} placeholder={this.props.content.text1}></input>
+      {this.state.searchResults.length ? <img onClick={() => this.setState({ searchResults: [], currentInput: '' })} src="/static/svg/Close-Cancel.svg" alt="" className="CloseTeachingSearch"></img> : null}
+      <div className="TeachingSearchItemDiv">
+        {this.state.searchResults.length ? <div className="TeachingSearchNumberOfVideos">{this.state.searchResults.length} Videos</div> : null}
+        {this.state.searchResults.length ? this.state.searchResults.map((item: any) => {
+          if (item.episodeTitle !== null)
+            return (
+              <div key={item.id} onClick={() => { this.openVideo(item) }} className="TeachingSearchResultItem">
+                <div className="Content">
+                  <div className="Details">
+                    <div className="Title">{item.episodeTitle}</div>
+                    <div className="RightContainer">
+                      {item.publishedDate ? <div className="Date">{item.publishedDate}</div> : null}
+                      {item.length ? <div className="Length">&nbsp;• {this.formatVideoDuration(item.length)}</div> : null}
+                    </div>
                   </div>
-                </div>)
-            else
-              return null
-          }) : null}
+                  <div className="Description">{item.description}</div>
+                </div>
+              </div>)
+          else
+            return null
+        }) : null}
       </div>
     </div>)
   }
