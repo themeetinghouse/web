7cbbd31c2722dee8fed0a00a0ae7237c108c7928
Nik Klassen <nikklassen@users.noreply.github.com>
2020-07-08 19:40:54 -0400

    Add types to Map components

---
+++ src/components/types.ts
@@ -0,0 +1,21 @@
+export interface SundayMorningItemContent {
+  type: 'sunday-morning';
+
+  alternate: string;
+
+  header1: string;
+}
+
+export interface DistanceGroupItemContent {
+  type: 'distance-groups';
+
+  header1: string;
+}
+
+export interface HomeChurchItemContent {
+  type: 'home-church';
+
+  class: 'home-church';
+
+  header1: string;
+}
