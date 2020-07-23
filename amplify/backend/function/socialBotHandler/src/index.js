58c93a07c9301f7dc6ebd438bd456557fd5d2d60
Jon Esparaz <jon.esparaz@gmail.com>
2020-07-23 01:34:06 -0400

    new lambda function for og tags

---
+++ amplify/backend/function/socialBotHandler/src/index.js
@@ -0,0 +1,16 @@
+/* Amplify Params - DO NOT EDIT
+	API_THEMEETINGHOUSE_GRAPHQLAPIENDPOINTOUTPUT
+	API_THEMEETINGHOUSE_GRAPHQLAPIIDOUTPUT
+	ENV
+	HOSTING_S3ANDCLOUDFRONT_HOSTINGBUCKETNAME
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
