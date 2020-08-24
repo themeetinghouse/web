0c5f7761d7b5b46b2fbb6344d33aff7ede4ca092
Jon Esparaz <jon.esparaz@gmail.com>
2020-08-24 12:59:54 -0400

    QandEh lambda fn for app

---
+++ amplify/backend/function/QandEh/src/index.js
@@ -0,0 +1,35 @@
+const aws = require('aws-sdk');
+const ses = new aws.SES();
+
+exports.handler = (event, context, callback) => {
+    const message = event.arguments.body;
+    console.log(message)
+    const params = {
+        Destination: {
+            ToAddresses: ["ask@themeetinghouse.com"]
+        },
+        ReplyToAddresses: [event.arguments.email],
+        Message: {
+            Body: {
+                Text: {
+                    Data: message
+                }
+            },
+            Subject: {
+                Data: 'Teaching Q&A'
+            }
+        },
+        Source: "donot-reply@themeetinghouse.com"
+    };
+
+    ses.sendEmail(params, function (err, data) {
+        callback(null, { err: err, data: data });
+        if (err) {
+            console.log(err);
+            context.fail(err);
+        } else {
+            console.log(data);
+            context.succeed(event);
+        }
+    });
+};
