fc21de967ca15319909b3b2af7e9259170792a7b
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2020-11-07 02:27:07 -0500

    app download badges

--- src/components/Menu/HomeFooter.tsx
+++ src/components/Menu/HomeFooter.tsx
@@ -1,119 +1,34 @@
-﻿//import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
+﻿import React from 'react';
 import { NavLink as RSNavLink } from 'reactstrap';
 import { isMobileOnly } from 'react-device-detect';
 import './HomeFooter.scss';
-//const bootstrap = require('react-bootstrap');
-//import Popper from 'popper.js'
-//import Head from 'next/head'
-//import "../../bootstrap-override.css"
-//import console = require('console');
-//import "./menu.scss"
 import { MDBRow, MDBCol, MDBFooter, MDBContainer } from 'mdbreact';
 import { NavLink } from 'components/Link/Link';
+
+const socialLinks = [
+  {
+    platform: 'Facebook',
+    link: 'https://www.facebook.com/themeetinghousechurch',
+    svg: '/static/svg/Facebook.svg',
+  },
+  {
+    platform: 'Twitter',
+    link: 'https://twitter.com/TheMeetingHouse',
+    svg: '/static/svg/Twitter.svg',
+  },
+  {
+    platform: 'Youtube',
+    link: 'https://www.youtube.com/themeetinghouse',
+    svg: '/static/svg/Youtube.svg',
+  },
+  {
+    platform: 'Instagram',
+    link: 'https://www.instagram.com/themeetinghouse',
+    svg: '/static/svg/Instagram.svg',
+  },
+];
+
 export default class HomeFooter extends React.Component {
-  iconHandler(item: boolean) {
-    if (!item) {
-      return ['Facebook', 'Twitter', 'YouTube', 'Instagram'];
-    } else {
-      return ['', '', '', ''];
-    }
-  }
-
-  renderIcons(links: string[]) {
-    return (
-      <div className="footerSocial">
-        <span>
-          <a
-            href="https://www.facebook.com/themeetinghousechurch/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="link-plain FooterSocialA"
-            style={{ whiteSpace: 'nowrap', color: '#1A1A1A' }}
-          >
-            <img
-              style={{
-                marginRight: '0.5vw',
-                marginTop: '2.5vw',
-                marginBottom: '2.5vw',
-              }}
-              className="FooterSocialImg"
-              src="/static/svg/Facebook.svg"
-              alt="Facebook Logo"
-            />
-            {links[0]}
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://twitter.com/TheMeetingHouse"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="link-plain FooterSocialA"
-            style={{ whiteSpace: 'nowrap', color: '#1A1A1A' }}
-          >
-            <img
-              className="FooterSocialImg"
-              style={{
-                marginRight: '0.5vw',
-                marginLeft: '3vw',
-                marginTop: '2.5vw',
-                marginBottom: '2.5vw',
-              }}
-              src="/static/svg/Twitter.svg"
-              alt="Twitter Logo"
-            />
-            {links[1]}
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://www.youtube.com/themeetinghouse"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="link-plain FooterSocialA"
-            style={{ whiteSpace: 'nowrap', color: '#1A1A1A' }}
-          >
-            <img
-              className="FooterSocialImg"
-              style={{
-                marginRight: '0.5vw',
-                marginLeft: '3vw',
-                marginTop: '2.5vw',
-                marginBottom: '2.5vw',
-              }}
-              src="/static/svg/Youtube.svg"
-              alt="Youtube Logo"
-            />
-            {links[2]}
-          </a>
-        </span>
-        <span>
-          <a
-            href="https://www.instagram.com/themeetinghouse/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="link-plain FooterSocialA"
-            style={{ whiteSpace: 'nowrap', color: '#1A1A1A' }}
-          >
-            <img
-              className="FooterSocialImg"
-              style={{
-                marginRight: '0.5vw',
-                marginLeft: '3vw',
-                marginTop: '2.5vw',
-                marginBottom: '2.5vw',
-              }}
-              src="/static/svg/Instagram.svg"
-              alt="Instagram Logo"
-            />
-            {links[3]}
-          </a>
-        </span>
-      </div>
-    );
-  }
-
   render() {
     return (
       <MDBFooter color="black" className="HomeFooter footer font-small ">
@@ -185,7 +100,58 @@ export default class HomeFooter extends React.Component {
           </MDBRow>
           <br />
           <br />
-          {this.renderIcons(this.iconHandler(isMobileOnly))}
+          <div className="FooterSocial">
+            {socialLinks.map((item) => {
+              return (
+                <a
+                  key={item.platform}
+                  href={item.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="link-plain FooterSocialA"
+                  style={{ whiteSpace: 'nowrap', color: '#1A1A1A' }}
+                >
+                  <img
+                    style={{
+                      marginRight: '0.5vw',
+                      marginTop: '2.5vw',
+                      marginBottom: '2.5vw',
+                    }}
+                    className="FooterSocialImg"
+                    src={item.svg}
+                    alt={item.platform + ' Logo'}
+                  />
+                  {isMobileOnly ? '' : item.platform}
+                </a>
+              );
+            })}
+          </div>
+          <div className="AppBadges">
+            <a
+              className="app-download-link"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://apps.apple.com/app/apple-store/id917315049?pt=1963922&ct=website&mt=8"
+            >
+              <img
+                src="/static/black-download-on-the-app-store.svg"
+                alt="Download on the App Store"
+                className="app-store-badge"
+              />
+            </a>
+            <a
+              className="app-download-link"
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://play.google.com/store/apps/details?id=org.tmh.takenote&utm_source=website&utm_medium=website"
+            >
+              <img
+                src="/static/google-play-badge.png"
+                alt="Get it on Google Play"
+                className="play-store-badge"
+              />
+            </a>
+          </div>
           <br />
           <br />
           <br />
