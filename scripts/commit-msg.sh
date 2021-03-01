d3673e15b61207f65e8fb1657fe19772840eae38
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-03-01 16:28:22 -0500

    Append [skip-cd] to merge commits

--- scripts/commit-msg.sh
+++ scripts/commit-msg.sh
@@ -2,7 +2,10 @@ set -e
 
 PUBLIC_STATIC=$(git diff --name-only --cached -- public/static)
 REPO=$(git diff --name-only --cached)
+COMMIT_MSG=$(cat $HUSKY_GIT_PARAMS)
 
-if [ "$PUBLIC_STATIC" == "$REPO" ] && [ -n "$PUBLIC_STATIC" ]; then
+if [[ $COMMIT_MSG == "Merge branch 'master' of"* ]]; then
+  echo "[skip-cd]" >> $HUSKY_GIT_PARAMS
+elif [ "$PUBLIC_STATIC" == "$REPO" ] && [ -n "$PUBLIC_STATIC" ]; then
   echo "[skip-cd]" >> $HUSKY_GIT_PARAMS
 fi
