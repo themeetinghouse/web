4f49e1dc53ef80c6ed7efafa7ee02527be9fe4b2
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-03-03 17:49:39 -0500

    refactor footer (#692)

--- src/components/Menu/HomeFooter.tsx
+++ src/components/Menu/HomeFooter.tsx
@@ -1,183 +1,157 @@
 ﻿import React from 'react';
-import { NavLink as RSNavLink } from 'reactstrap';
-import { isMobileOnly } from 'react-device-detect';
-import './HomeFooter.scss';
-import { MDBRow, MDBCol, MDBFooter, MDBContainer } from 'mdbreact';
+import { NavLink as RSNavLink, Container, Row, Col } from 'reactstrap';
 import { NavLink } from 'components/Link/Link';
+import './HomeFooter.scss';
 
-const socialLinks = [
-  {
-    platform: 'Facebook',
-    link: 'https://www.facebook.com/themeetinghousechurch',
-    svg: '/static/svg/Facebook.svg',
-  },
-  {
-    platform: 'Twitter',
-    link: 'https://twitter.com/TheMeetingHouse',
-    svg: '/static/svg/Twitter.svg',
-  },
-  {
-    platform: 'Youtube',
-    link: 'https://www.youtube.com/themeetinghouse',
-    svg: '/static/svg/Youtube.svg',
-  },
-  {
-    platform: 'Instagram',
-    link: 'https://www.instagram.com/themeetinghouse',
-    svg: '/static/svg/Instagram.svg',
-  },
-  {
-    platform: 'TikTok',
-    link: 'https://www.tiktok.com/@themeetinghouse?lang=en',
-    svg: '/static/svg/tiktok.svg',
-  },
-  {
-    platform: 'Discord',
-    link: 'https://www.tinyurl.com/TMH-discord',
-    svg: '/static/svg/Discord.svg',
-  },
-];
+export default function HomeFooter() {
+  const socialLinks = [
+    {
+      platform: 'Facebook',
+      link: 'https://www.facebook.com/themeetinghousechurch',
+      svg: '/static/svg/Facebook.svg',
+    },
+    {
+      platform: 'Twitter',
+      link: 'https://twitter.com/TheMeetingHouse',
+      svg: '/static/svg/Twitter.svg',
+    },
+    {
+      platform: 'Youtube',
+      link: 'https://www.youtube.com/themeetinghouse',
+      svg: '/static/svg/Youtube.svg',
+    },
+    {
+      platform: 'Instagram',
+      link: 'https://www.instagram.com/themeetinghouse',
+      svg: '/static/svg/Instagram.svg',
+    },
+    {
+      platform: 'TikTok',
+      link: 'https://www.tiktok.com/@themeetinghouse?lang=en',
+      svg: '/static/svg/tiktok.svg',
+    },
+    {
+      platform: 'Discord',
+      link: 'https://www.tinyurl.com/TMH-discord',
+      svg: '/static/svg/Discord.svg',
+    },
+  ];
 
-export default class HomeFooter extends React.Component {
-  render() {
-    return (
-      <MDBFooter color="black" className="HomeFooter footer font-small ">
-        <MDBContainer fluid className="text-center text-md-left">
-          <MDBRow>
-            <MDBCol md="6">
-              <div style={{ textAlign: 'left' }}>
-                <NavLink className="FooterNavLink" to="/connect">
-                  Connect
-                </NavLink>
-                <NavLink className="FooterNavLink" to="/about-us">
-                  About Us
-                </NavLink>
-                <NavLink className="FooterNavLink" to="/careers">
-                  Staff &amp; Careers
-                </NavLink>
-                <RSNavLink
-                  className="FooterNavLink"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="http://www.tmhrentals.com"
-                >
-                  Oakville Facility Rentals
-                </RSNavLink>
-              </div>
-              <br />
-              <br />
-            </MDBCol>
-            <MDBCol md="6">
-              <p
-                className="HomeFooterP"
-                style={{
-                  lineHeight: '16px',
-                  fontSize: '12px',
-                  fontFamily: 'Graphik Web',
-                  color: '#1A1A1A',
-                }}
+  return (
+    <div className="HomeFooter footer">
+      <Container fluid>
+        <Row>
+          <Col md="6">
+            <div className="text-left">
+              <NavLink className="FooterNavLink" to="/connect">
+                Connect
+              </NavLink>
+              <NavLink className="FooterNavLink" to="/about-us">
+                About Us
+              </NavLink>
+              <NavLink className="FooterNavLink" to="/careers">
+                Staff &amp; Careers
+              </NavLink>
+              <RSNavLink
+                className="FooterNavLink"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="http://www.tmhrentals.com"
               >
-                <b>Main Office</b>
-                <br />
-                <a
-                  href="https://goo.gl/maps/TqH1eGQ4pDxDZmqr7"
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  2700 Bristol Circle
-                </a>
-                <br />
-                Oakville, ON L6H 6E1
-                <br />
-                Canada
-                <br />
-                <br />
-                <b>Phone</b>
-                <br />
-                <a href="tel:+1-905-287-7000">905-287-7000</a>
-                <br />
-                <br />
-                <b>Email</b>
-                <br />
-                <a href="mailto:hello@themeetinghouse.com">
-                  hello@themeetinghouse.com
-                </a>
-              </p>
-            </MDBCol>
-          </MDBRow>
-          <br />
-          <br />
-          <div className="FooterSocial">
-            {socialLinks.map((item) => {
-              return (
-                <a
-                  key={item.platform}
-                  href={item.link}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                  className="link-plain FooterSocialA"
-                  style={{ whiteSpace: 'nowrap', color: '#1A1A1A' }}
-                >
-                  <img
-                    style={{
-                      marginRight: '0.5vw',
-                      marginTop: '2.5vw',
-                      marginBottom: '2.5vw',
-                    }}
-                    className="FooterSocialImg"
-                    src={item.svg}
-                    alt={item.platform + ' Logo'}
-                  />
-                  {isMobileOnly ? '' : item.platform}
-                </a>
-              );
-            })}
-          </div>
-          <div className="AppBadges">
-            <a
-              className="app-download-link"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://apps.apple.com/app/apple-store/id917315049?pt=1963922&ct=website&mt=8"
-            >
-              <img
-                src="/static/black-download-on-the-app-store.svg"
-                alt="Download on the App Store"
-                className="app-store-badge"
-              />
-            </a>
-            <a
-              className="app-download-link"
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://play.google.com/store/apps/details?id=org.tmh.takenote&utm_source=website&utm_medium=website"
-            >
-              <img
-                src="/static/google-play-badge.png"
-                alt="Get it on Google Play"
-                className="play-store-badge"
-              />
-            </a>
-          </div>
-          <br />
-          <br />
-          <br />
-          <p
-            className="HomeFooterCopyright"
-            style={{
-              fontFamily: 'Graphik Web',
-              fontSize: '12px',
-              margin: '0px',
-              padding: '5px',
-              color: '#646469',
-            }}
+                Oakville Facility Rentals
+              </RSNavLink>
+            </div>
+            <br />
+            <br />
+          </Col>
+          <Col md="6">
+            <p className="HomeFooterP">
+              <b>Main Office</b>
+              <br />
+              <a
+                href="https://goo.gl/maps/TqH1eGQ4pDxDZmqr7"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                2700 Bristol Circle
+              </a>
+              <br />
+              Oakville, ON L6H 6E1
+              <br />
+              Canada
+              <br />
+              <br />
+              <b>Phone</b>
+              <br />
+              <a href="tel:+1-905-287-7000">905-287-7000</a>
+              <br />
+              <br />
+              <b>Email</b>
+              <br />
+              <a href="mailto:hello@themeetinghouse.com">
+                hello@themeetinghouse.com
+              </a>
+            </p>
+          </Col>
+        </Row>
+        <br />
+        <br />
+        <div className="FooterSocial">
+          {socialLinks.map((item) => {
+            return (
+              <a
+                key={item.platform}
+                href={item.link}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="link-plain FooterSocialA"
+              >
+                <img
+                  className="FooterSocialImg"
+                  src={item.svg}
+                  alt={item.platform + ' Logo'}
+                  width={24}
+                  height={24}
+                />
+                <span className="social-platform-name">{item.platform}</span>
+              </a>
+            );
+          })}
+        </div>
+        <div className="AppBadges">
+          <a
+            className="app-download-link"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://apps.apple.com/app/apple-store/id917315049?pt=1963922&ct=website&mt=8"
           >
-            {' '}
-            All Rights Reserved. &copy; {new Date().getFullYear()} The Meeting
-            House
-          </p>
-        </MDBContainer>
-      </MDBFooter>
-    );
-  }
+            <img
+              src="/static/black-download-on-the-app-store.svg"
+              alt="Download on the App Store"
+              className="app-store-badge"
+            />
+          </a>
+          <a
+            className="app-download-link"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://play.google.com/store/apps/details?id=org.tmh.takenote&utm_source=website&utm_medium=website"
+          >
+            <img
+              src="/static/google-play-badge.png"
+              alt="Get it on Google Play"
+              className="play-store-badge"
+            />
+          </a>
+        </div>
+        <br />
+        <br />
+        <br />
+        <p className="HomeFooterCopyright">
+          All Rights Reserved. &copy; {new Date().getFullYear()} The Meeting
+          House
+        </p>
+      </Container>
+    </div>
+  );
 }
