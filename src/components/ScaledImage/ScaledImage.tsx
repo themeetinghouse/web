82cf641c7eee717d68979d21709cc5da9ff4cdbe
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-03-18 01:25:13 -0400

    Serve WebP images via <picture> tag (#699)
    * serve webp images via <picture> tag
    
    * fix URI encoding bug

--- src/components/ScaledImage/ScaledImage.tsx
+++ src/components/ScaledImage/ScaledImage.tsx
@@ -3,6 +3,7 @@ import {
   EventHandler,
   ReactElement,
   ImgHTMLAttributes,
+  useState,
 } from 'react';
 import { ItemImage } from 'components/types';
 
@@ -10,6 +11,10 @@ interface Props extends ImgHTMLAttributes<HTMLImageElement> {
   image?: ItemImage;
   fallbackUrl?: string;
   breakpointSizes: { [maxWidth: string]: number };
+}
+
+function encodeImageURI(s: string) {
+  return encodeURIComponent(s).replace(/%2F/g, '/');
 }
 
 export function tmhImageUrl(size: number, imageSrc: string) {
@@ -21,7 +26,15 @@ export function tmhImageUrl(size: number, imageSrc: string) {
   } else {
     baseUrl = 'https://www.themeetinghouse.com/cache/' + size;
   }
-  return `${baseUrl}${imageSrc} ${size}w`;
+  return `${baseUrl}${encodeImageURI(imageSrc)} ${size}w`;
+}
+
+function tmhWebpUrl(size: number, imageSrc: string) {
+  const fileExt = imageSrc.substring(imageSrc.lastIndexOf('.'));
+
+  return `https://www.themeetinghouse.com/cache/${size}webp${encodeImageURI(
+    imageSrc.replace(fileExt, '.webp')
+  )} ${size}w`;
 }
 
 export function fallbackToImage(
@@ -39,6 +52,8 @@ export function fallbackToImage(
 }
 
 export default function ScaledImage(props: Props): ReactElement<Props> | null {
+  const [isError, setIsError] = useState(false);
+
   const {
     image,
     fallbackUrl,
@@ -71,6 +86,10 @@ export default function ScaledImage(props: Props): ReactElement<Props> | null {
     .map(([, size]) => tmhImageUrl(size, image.src))
     .join(',');
 
+  const webpSrcSet = imageSizes
+    .map(([, size]) => tmhWebpUrl(size, image.src))
+    .join(',');
+
   let sizesAttr = imageSizes
     .slice(0, imageSizes.length - 2)
     .map(([breakpoint, size]) => `(max-width: ${breakpoint}px) ${size}px`)
@@ -80,13 +99,19 @@ export default function ScaledImage(props: Props): ReactElement<Props> | null {
 
   const uri = tmhImageUrl(largestSize, image.src);
   return (
-    <img
-      src={uri.substring(0, uri.lastIndexOf(' ') + 1)}
-      alt={image.alt}
-      onError={fallbackToImage(fallbackUrl)}
-      srcSet={srcSet}
-      sizes={sizesAttr}
-      {...htmlProps}
-    />
+    <picture>
+      {!isError && (
+        <>
+          <source srcSet={webpSrcSet} sizes={sizesAttr} type="image/webp" />
+          <source srcSet={srcSet} sizes={sizesAttr} type="image/jpeg" />
+        </>
+      )}
+      <img
+        src={isError ? fallbackUrl : uri.substring(0, uri.lastIndexOf(' ') + 1)}
+        alt={image.alt}
+        onError={() => setIsError(true)}
+        {...htmlProps}
+      />
+    </picture>
   );
 }
