7b1f31b9fe597536c3c0aab239394771e618db73
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-04-27 12:04:48 -0400

    Force [skip-cd] to new line

--- scripts/commit-msg.sh
+++ scripts/commit-msg.sh
@@ -4,8 +4,10 @@ PUBLIC_STATIC=$(git diff --name-only --cached -- public/static)
 REPO=$(git diff --name-only --cached)
 COMMIT_MSG=$(cat $HUSKY_GIT_PARAMS)
 
+SKIP_CD=$'\n[skip-cd]'
+
 if [[ $COMMIT_MSG == "Merge branch 'master' of"* ]]; then
-  echo "[skip-cd]" >> $HUSKY_GIT_PARAMS
+  echo "$SKIP_CD" >> $HUSKY_GIT_PARAMS
 elif [ "$PUBLIC_STATIC" == "$REPO" ] && [ -n "$PUBLIC_STATIC" ]; then
-  echo "[skip-cd]" >> $HUSKY_GIT_PARAMS
+  echo "$SKIP_CD" >> $HUSKY_GIT_PARAMS
 fi
