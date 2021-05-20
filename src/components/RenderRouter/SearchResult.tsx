c5d04daf2423826bd1d6c847e1efbf9139999cbb
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2021-05-20 04:40:05 -0400

    Remove state from searchresults

--- src/components/RenderRouter/SearchResult.tsx
+++ src/components/RenderRouter/SearchResult.tsx
@@ -6,18 +6,10 @@ import { Link } from 'components/Link/Link';
 interface Props {
   content: any;
 }
-interface State {
-  content: any;
-  currentPage: any;
-}
 
-export default class ContentItem extends React.Component<Props, State> {
+export default class ContentItem extends React.Component<Props> {
   constructor(props: Props) {
     super(props);
-    this.state = {
-      content: props.content,
-      currentPage: 'PushPay',
-    };
   }
   imgUrl(size: any) {
     if (window.location.hostname === 'localhost')
@@ -29,13 +21,13 @@ export default class ContentItem extends React.Component<Props, State> {
 
   render() {
     const image = {
-      src: `${this.state.content.image}`,
-      alt: `${this.state.content.imageAlt}`,
+      src: `${this.props.content.image}`,
+      alt: `${this.props.content.imageAlt}`,
     };
     return (
       <div className="searchResultDiv1">
-        <Link to={this.state.content.navigateTo}>
-          <div key={this.state.content.header} className="SearchResultItem">
+        <Link to={this.props.content.navigateTo}>
+          <div key={this.props.content.header} className="SearchResultItem">
             <ScaledImage
               image={image}
               className="StaffImage"
@@ -50,8 +42,8 @@ export default class ContentItem extends React.Component<Props, State> {
               }}
             />
             <div className="Content">
-              <div className="Title">{this.state.content.header1}</div>
-              <div className="Description">{this.state.content.text1}</div>
+              <div className="Title">{this.props.content.header1}</div>
+              <div className="Description">{this.props.content.text1}</div>
             </div>
           </div>
         </Link>
