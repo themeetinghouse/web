3da4b8e2d0cf7c8df3c75a7287506094f03aa489
Igor Belem <igortbelem@gmail.com>
2021-07-27 09:05:26 -0400

    Enabled footer, Removed auth check useEffect hook, now using userState.authState

--- src/pages/users/AccountMain.tsx
+++ src/pages/users/AccountMain.tsx
@@ -6,10 +6,8 @@ import DashboardHome from './DashboardHome/DashboardHome';
 import GivePage from './Give/GivePage';
 import PaymentMethodsPage from './PaymentMethods/PaymentMethodsPage';
 import TransactionsPage from './Transactions/TransactionsPage';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import ProfilePage from './ProfilePage/ProfilePage';
-import { useEffect } from 'react';
-import { Auth } from 'aws-amplify';
 import { UserContext } from 'components/Auth/UserContext';
 import Admin from '../admin/index';
 import { Elements } from '@stripe/react-stripe-js';
@@ -23,8 +21,7 @@ else env = 'prod';
 export default function AccountMain(): JSX.Element {
   const history = useHistory();
   const [open, setOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const UserConsumer = UserContext.Consumer;
+  const { userState } = useContext(UserContext);
   const [stripePromise] = useState(() =>
     loadStripe(
       env == 'beta'
@@ -32,82 +29,57 @@ export default function AccountMain(): JSX.Element {
         : 'pk_test_51HlyrYLTzrDhiQ921sERNUY2GQBDgpHDOUYMiNZ0lTeTsse9u8oQoBfLg6UzWaxcNkYhek4tkNWILTlAiajet27k00FFv6z0RB'
     )
   );
-  useEffect(() => {
-    const checkAuthStatus = async () => {
-      try {
-        const user = await Auth.currentAuthenticatedUser();
-        const session = await Auth.userSession(user);
-        if (!session.isValid()) {
-          setIsLoggedIn(false);
-          console.log('invalid session');
-          history.push('/signin');
-        } else {
-          console.log('Valid session');
-          setIsLoggedIn(true);
-        }
-      } catch (err) {
-        console.log(err);
-        history.push('/signin');
-      }
-    };
-    checkAuthStatus();
-  });
-  return isLoggedIn ? (
-    <UserConsumer>
-      {({ userState }) => {
-        if (!userState) return null;
-        console.log(userState.groups);
-        return (
-          <div className="MyAccountContainer">
-            <Elements stripe={stripePromise}>
-              <MyAccountNav
-                navigationItems={[
-                  ...[
-                    { label: 'Home', link: '/' },
-                    { label: 'Give', link: '/give' },
-                    { label: 'Transactions', link: '/transactions' },
-                    { label: 'Payment Methods', link: '/payments' },
-                    { label: 'Profile', link: '/profile' },
-                  ],
-                  ...(userState.groups?.includes('Admin')
-                    ? [
-                        {
-                          label: 'Admin',
-                          link: '/admin',
-                        },
-                      ]
-                    : []),
-                ]}
-                open={open}
-                toggle={() => setOpen((prev) => !prev)}
+  if (!userState || userState.authState !== 'signedIn') {
+    console.log('userState', userState);
+    history.push('/signin');
+    return <p>Redirecting</p>;
+  } else
+    return (
+      <div className="MyAccountContainer">
+        <Elements stripe={stripePromise}>
+          <MyAccountNav
+            navigationItems={[
+              ...[
+                { label: 'Home', link: '/' },
+                { label: 'Give', link: '/give' },
+                { label: 'Transactions', link: '/transactions' },
+                { label: 'Payment Methods', link: '/payments' },
+                { label: 'Profile', link: '/profile' },
+              ],
+              ...(userState.groups?.includes('Admin')
+                ? [
+                    {
+                      label: 'Admin',
+                      link: '/admin',
+                    },
+                  ]
+                : []),
+            ]}
+            open={open}
+            toggle={() => setOpen((prev) => !prev)}
+          />
+          <div
+            className="DashboardContent"
+            style={open ? { display: 'none' } : {}}
+          >
+            <Switch>
+              <Route path={`/account`} component={DashboardHome} exact />
+              <Route path={`/account/give`} component={GivePage} />
+              <Route
+                path={`/account/transactions`}
+                component={TransactionsPage}
               />
-              <div
-                className="DashboardContent"
-                style={open ? { display: 'none' } : {}}
-              >
-                <Switch>
-                  <Route path={`/account`} component={DashboardHome} exact />
-                  <Route path={`/account/give`} component={GivePage} />
-                  <Route
-                    path={`/account/transactions`}
-                    component={TransactionsPage}
-                  />
-                  <Route
-                    path={`/account/payments`}
-                    component={PaymentMethodsPage}
-                  />
-                  <Route path={`/account/profile`} component={ProfilePage} />
-                  <Route path={`/account/admin`} component={Admin} />
-                </Switch>
-              </div>
-
-              {false ? <DashboardFooter /> : null}
-            </Elements>
+              <Route
+                path={`/account/payments`}
+                component={PaymentMethodsPage}
+              />
+              <Route path={`/account/profile`} component={ProfilePage} />
+              <Route path={`/account/admin`} component={Admin} />
+            </Switch>
           </div>
-        );
-      }}
-    </UserConsumer>
-  ) : (
-    <div />
-  );
+
+          <DashboardFooter />
+        </Elements>
+      </div>
+    );
 }
