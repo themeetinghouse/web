5e2776aa48b39199b5de9248a4861ce96583f8c9
Igor Belem <igortbelem@gmail.com>
2021-07-27 08:35:34 -0400

    Fixed footer styling

--- src/pages/users/DashboardFooter.tsx
+++ src/pages/users/DashboardFooter.tsx
@@ -5,12 +5,18 @@ export default function DashboardFooter(): JSX.Element {
       <div style={{ flexDirection: 'column', margin: '0px 32px 0px 0px' }}>
         <p className="footerText">2020 Tax Receipt</p>
         <p className="subText">
-          Download <a href="/">here</a>
+          Download <a href="">here</a>
         </p>
       </div>
       <div style={{ flexDirection: 'column', margin: '0px 32px 0px 0px' }}>
         <p className="footerText">Need help?</p>
-        <p className="subText">donate@themeetinghouse.com</p>
+        <a
+          href="mailto: donate@themeetinghouse.com"
+          className="subText"
+          style={{ borderBottom: 'none' }}
+        >
+          donate@themeetinghouse.com
+        </a>
       </div>
     </div>
   );
