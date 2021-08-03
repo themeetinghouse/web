7b2c0a85487996195b4e460d0193843c650356d9
Igor Belem <igortbelem@gmail.com>
2021-08-02 22:44:06 -0400

    created fund selection dropdown component

---
+++ src/pages/users/Give/GiveSelect.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import './GiveSelect.scss';
+type GiveSelectProps = {
+  form: any;
+  setForm: (a: any) => void; //setState type
+};
+export default function GiveSelect(props: GiveSelectProps) {
+  const { form, setForm } = props;
+  const [fundOptions, setFundOptions] = useState<Array<string>>([]);
+  useEffect(() => {
+    const loadFundOptions = async () => {
+      setTimeout(() => {
+        setFundOptions([
+          'Please make a selection',
+          'General',
+          'Compassion',
+          'Go',
+          'Curriculum',
+        ]);
+      }, 500);
+    };
+    loadFundOptions();
+  }, []);
+  return (
+    <select
+      value={form.fund.name}
+      onChange={(e) => setForm({ ...form, fund: { name: e.target.value } })}
+      className="GiveInput"
+      style={{ padding: '0px 30px' }}
+    >
+      {!fundOptions.length ? (
+        <option value="loading">Loading options...</option>
+      ) : (
+        fundOptions.map((fundName) => {
+          return (
+            <option key={fundName} value={fundName}>
+              {fundName} fund
+            </option>
+          );
+        })
+      )}
+    </select>
+  );
+}
