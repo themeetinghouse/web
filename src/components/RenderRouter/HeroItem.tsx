2bc333665650f87a2bf968fcca45f2b1dff853d0
Igor Belem <igortbelem@gmail.com>
2021-08-17 13:38:16 -0400

    Added secondary CTA for hero style full (#820)

--- src/components/RenderRouter/HeroItem.tsx
+++ src/components/RenderRouter/HeroItem.tsx
@@ -232,6 +232,30 @@ class HeroItem extends React.Component<Props, State> {
       </Button>
     ) : null;
   }
+
+  renderSecondaryCTA(buttonInfo: any) {
+    if (!buttonInfo) return null;
+    return (
+      <Link
+        to={buttonInfo.action}
+        className="secondaryCTA"
+        style={{
+          marginTop: 20,
+        }}
+        aria-label={buttonInfo.description}
+      >
+        {buttonInfo.icon ? (
+          <img
+            className="calendarImage"
+            src={buttonInfo.icon}
+            alt={`${buttonInfo.text} Icon`}
+          />
+        ) : null}
+        {buttonInfo.text}
+      </Link>
+    );
+  }
+
   render() {
     window.onscroll = () => {
       this.downArrowScroll();
@@ -279,7 +303,7 @@ class HeroItem extends React.Component<Props, State> {
                     height={25}
                     className="calendarImage"
                     src="/static/svg/Play.svg"
-                    alt="Contact Icon"
+                    alt="Play Icon"
                   />
                   Watch Live
                 </Link>
@@ -295,7 +319,7 @@ class HeroItem extends React.Component<Props, State> {
                     height={25}
                     className="calendarImage"
                     src="/static/svg/Play.svg"
-                    alt="Contact Icon"
+                    alt="Play Icon"
                   />
                   Watch Live
                 </Link>
@@ -344,6 +368,8 @@ class HeroItem extends React.Component<Props, State> {
                     />
                   )
                 ) : null
+              ) : this.state.content.secondaryCTA ? (
+                this.renderSecondaryCTA(this.state.content.secondaryCTA)
               ) : null}
               {this.state.content.contactPastor ? (
                 this.state.locationData.length === 1 ? (
