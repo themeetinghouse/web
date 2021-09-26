2e73139b2440e6c0b46649743c1cadacaeaf9dda
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-09-26 12:36:59 -0400

    improved handling of whitespace and errors in verse parser for notes

---
+++ src/pages/admin/__tests__/verses.spec.js
@@ -0,0 +1,372 @@
+import { getQueryString } from '../verses';
+
+const validReferences = [
+  {
+    reference: 'Genesis 3',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.NIV',
+      queryString: 'GEN.3',
+    },
+  },
+  {
+    reference: 'genesis 3',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.NIV',
+      queryString: 'GEN.3',
+    },
+  },
+  {
+    reference: 'Genesis 3-4',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.NIV',
+      queryString: 'GEN.3-GEN.4',
+    },
+  },
+  {
+    reference: 'Genesis 3:1-4',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.1-4.NIV',
+      queryString: 'GEN.3.1-GEN.3.4',
+    },
+  },
+  {
+    reference: 'genesis 3:1-4',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.1-4.NIV',
+      queryString: 'GEN.3.1-GEN.3.4',
+    },
+  },
+  {
+    reference: 'Genesis 3:20-4:20',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.NIV',
+      queryString: 'GEN.3.20-GEN.4.20',
+    },
+  },
+  {
+    reference: 'genesis 3:20-4:20',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/GEN.3.NIV',
+      queryString: 'GEN.3.20-GEN.4.20',
+    },
+  },
+  {
+    reference: '2 Peter 2',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/2PE.2.NIV',
+      queryString: '2PE.2',
+    },
+  },
+  {
+    reference: '1 Corinthians 8:4',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1CO.8.4.NIV',
+      queryString: '1CO.8.4',
+    },
+  },
+  {
+    reference: '1 Timothy 6:3-4',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1TI.6.3-4.NIV',
+      queryString: '1TI.6.3-1TI.6.4',
+    },
+  },
+  {
+    reference: '1 Corinthians 8:10-9:7',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1CO.8.NIV',
+      queryString: '1CO.8.10-1CO.9.7',
+    },
+  },
+  {
+    testDescription: '1 Corinthians 8:10 - 9:7 [extra spaces]',
+    reference: '1 Corinthians 8:10 - 9:7',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1CO.8.NIV',
+      queryString: '1CO.8.10-1CO.9.7',
+    },
+  },
+  {
+    testDescription: '1 Corinthians  8:10-9:7 [extra space]',
+    reference: '1 Corinthians  8:10-9:7',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1CO.8.NIV',
+      queryString: '1CO.8.10-1CO.9.7',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10: 24-25 [extra space]',
+    reference: 'Hebrews 10: 24-25',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10 :24-25 [extra space]',
+    reference: 'Hebrews 10 :24-25',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10:24-25 [leading whitespace]',
+    reference: ' Hebrews 10: 24-25',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10:24-25 [trailing whitespace]',
+    reference: 'Hebrews 10: 24-25  ',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10: 24 -25 [extra spaces]',
+    reference: 'Hebrews 10: 24 -25',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10:24 -   25 [extra spaces]',
+    reference: 'Hebrews 10:24 -   25',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    testDescription: 'Hebrews 10   :  24 -  25 [extra spaces]',
+    reference: 'Hebrews 10   :  24 -  25',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/HEB.10.24-25.NIV',
+      queryString: 'HEB.10.24-HEB.10.25',
+    },
+  },
+  {
+    reference: 'Song of Solomon 2:1',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/SNG.2.1.NIV',
+      queryString: 'SNG.2.1',
+    },
+  },
+  {
+    reference: 'Song of Songs 2:1',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/SNG.2.1.NIV',
+      queryString: 'SNG.2.1',
+    },
+  },
+  {
+    reference: 'Song 2:1',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/SNG.2.1.NIV',
+      queryString: 'SNG.2.1',
+    },
+  },
+  {
+    reference: 'Songs 2:1',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/SNG.2.1.NIV',
+      queryString: 'SNG.2.1',
+    },
+  },
+  {
+    reference: 'Solomon 2:1',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/SNG.2.1.NIV',
+      queryString: 'SNG.2.1',
+    },
+  },
+  {
+    reference: 'Psalms 128:6-130:4',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/PSA.128.NIV',
+      queryString: 'PSA.128.6-PSA.130.4',
+    },
+  },
+  {
+    reference: 'Matthew 5:14-16',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/MAT.5.14-16.NIV',
+      queryString: 'MAT.5.14-MAT.5.16',
+    },
+  },
+  {
+    reference: 'Ephesians 2:14-15',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/EPH.2.14-15.NIV',
+      queryString: 'EPH.2.14-EPH.2.15',
+    },
+  },
+  {
+    reference: 'Romans 15:14',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/ROM.15.14.NIV',
+      queryString: 'ROM.15.14',
+    },
+  },
+  {
+    reference: 'Colossians 3:16',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/COL.3.16.NIV',
+      queryString: 'COL.3.16',
+    },
+  },
+  {
+    reference: '1 Thessalonians 4:18',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1TH.4.18.NIV',
+      queryString: '1TH.4.18',
+    },
+  },
+  {
+    reference: 'Luke 6:37-39',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/LUK.6.37-39.NIV',
+      queryString: 'LUK.6.37-LUK.6.39',
+    },
+  },
+  {
+    reference: 'James 5:16',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/JAS.5.16.NIV',
+      queryString: 'JAS.5.16',
+    },
+  },
+  {
+    reference: '1 Peter 4:10',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1PE.4.10.NIV',
+      queryString: '1PE.4.10',
+    },
+  },
+  {
+    reference: '1 peter 4:10',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1PE.4.10.NIV',
+      queryString: '1PE.4.10',
+    },
+  },
+  {
+    reference: '1 PETER 4:10',
+    expected: {
+      youVersionUri: 'https://www.bible.com/bible/111/1PE.4.10.NIV',
+      queryString: '1PE.4.10',
+    },
+  },
+];
+
+const invalidReferences = [
+  {
+    testDescription: '1 Corinthians 8:10—9:7 [emdash]',
+    reference: '1 Corinthians 8:10—9:7',
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'Mark 7:1—2 [emdash]',
+    reference: 'Mark 7:1—2',
+    expected: 'invalid',
+  },
+  {
+    testDescription: '1 Corinthians 8:10–9:7 [endash]',
+    reference: '1 Corinthians 8:10–9:7',
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'Mark 7:1–2 [endash]',
+    reference: 'Mark 7:1–2',
+    expected: 'invalid',
+  },
+  {
+    testDescription: '1 Corinthians 8;10-9:7 [invalid character]',
+    reference: '1 Corinthians 8;10-9:7',
+    expected: 'invalid',
+  },
+  {
+    testDescription: '1 Corinthians 8:10=9:7 [invalid character]',
+    reference: '1 Corinthians 8:10=9:7',
+    expected: 'invalid',
+  },
+  {
+    testDescription: '1 Corinthians 8:10_9:7 [invalid character]',
+    reference: '1 Corinthians 8:10_9:7',
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'Matthew 7:21, 24-27 [invalid format]',
+    reference: 'Matthew 7:21, 24-27',
+    expected: 'invalid',
+  },
+  {
+    testDescription: '1:1 Genesis [invalid format]',
+    reference: '1:1 Genesis',
+    expected: 'invalid',
+  },
+  { reference: 'Jon 3:16', expected: 'invalid' },
+  { reference: 'Lucas 10:1-11:15', expected: 'invalid' },
+  { reference: '🔥 12', expected: 'invalid' },
+  { reference: '🔥🔥🔥', expected: 'invalid' },
+  { reference: '😀', expected: 'invalid' },
+  { reference: '😀 10:27', expected: 'invalid' },
+  { reference: '10:27', expected: 'invalid' },
+  { reference: '10:27-11:2', expected: 'invalid' },
+  { reference: 'abcdef%%61239asdkji', expected: 'invalid' },
+  { testDescription: 'Empty string', reference: '', expected: 'invalid' },
+  { testDescription: 'Array', reference: [], expected: 'invalid' },
+  {
+    testDescription: 'Function',
+    reference: () => console.log('test'),
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'Object',
+    reference: { a: 1, b: 2 },
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'Map',
+    reference: new Map(),
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'Number',
+    reference: 107,
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'null',
+    reference: null,
+    expected: 'invalid',
+  },
+  {
+    testDescription: 'undefined',
+    reference: undefined,
+    expected: 'invalid',
+  },
+];
+
+describe('getQueryString', () => {
+  describe('Parses valid references; returns object containing youVersionUri and queryString', () => {
+    validReferences.forEach(({ testDescription, reference, expected }) => {
+      test(testDescription ?? reference, () => {
+        expect(getQueryString(reference)).toEqual(expected);
+      });
+    });
+  });
+
+  describe("Does not parse invalid references; returns 'invalid'", () => {
+    invalidReferences.forEach(({ testDescription, reference, expected }) => {
+      test(testDescription ?? reference, () => {
+        expect(getQueryString(reference)).toEqual(expected);
+      });
+    });
+  });
+});
---
+++ src/pages/admin/verses.ts
@@ -0,0 +1,233 @@
+import { RawDraftContentState } from 'draft-js';
+
+type DataFromJSON = Array<{
+  passageRef: string;
+  key: string;
+  offset: number;
+  length: number;
+}>;
+
+const bookCodes: Record<string, string> = {
+  genesis: 'GEN',
+  exodus: 'EXO',
+  leviticus: 'LEV',
+  numbers: 'NUM',
+  deuteronomy: 'DEU',
+  joshua: 'JOS',
+  judges: 'JDG',
+  ruth: 'RUT',
+  '1 samuel': '1SA',
+  '2 samuel': '2SA',
+  '1 kings': '1KI',
+  '2 kings': '2KI',
+  '1 chronicles': '1CH',
+  '2 chronicles': '2CH',
+  ezra: 'EZR',
+  nehemiah: 'NEH',
+  esther: 'EST',
+  job: 'JOB',
+  psalms: 'PSA',
+  proverbs: 'PRO',
+  ecclesiastes: 'ECC',
+  solomon: 'SNG',
+  songs: 'SNG',
+  'song of songs': 'SNG',
+  'song of solomon': 'SNG',
+  song: 'SNG',
+  isaiah: 'ISA',
+  jeremiah: 'JER',
+  lamentations: 'LAM',
+  ezekiel: 'EZK',
+  daniel: 'DAN',
+  hosea: 'HOS',
+  joel: 'JOL',
+  amos: 'AMO',
+  obadiah: 'OBA',
+  jonah: 'JON',
+  micah: 'MIC',
+  nahum: 'NAM',
+  habakkuk: 'HAB',
+  zephaniah: 'ZEP',
+  haggai: 'HAG',
+  zechariah: 'ZEC',
+  malachi: 'MAL',
+  matthew: 'MAT',
+  mark: 'MRK',
+  luke: 'LUK',
+  john: 'JHN',
+  acts: 'ACT',
+  romans: 'ROM',
+  '1 corinthians': '1CO',
+  '2 corinthians': '2CO',
+  galatians: 'GAL',
+  ephesians: 'EPH',
+  philippians: 'PHP',
+  colossians: 'COL',
+  '1 thessalonians': '1TH',
+  '2 thessalonians': '2TH',
+  '1 timothy': '1TI',
+  '2 timothy': '2TI',
+  titus: 'TIT',
+  philemon: 'PHM',
+  hebrews: 'HEB',
+  james: 'JAS',
+  '1 peter': '1PE',
+  '2 peter': '2PE',
+  '1 john': '1JN',
+  '2 john': '2JN',
+  '3 john': '3JN',
+  jude: 'JUD',
+  revelation: 'REV',
+};
+
+export const INVALID_QUERY = 'invalid';
+
+export const getQueryString = (
+  reference: string
+): { youVersionUri: string; queryString: string } | typeof INVALID_QUERY => {
+  try {
+    const isValidReference =
+      typeof reference === 'string' && reference.length > 0;
+
+    if (!isValidReference) {
+      return INVALID_QUERY;
+    }
+
+    const referenceLowercase = reference.trim().toLowerCase();
+    const colons = (referenceLowercase.match(/:/g) || []).length;
+    const hyphens = (referenceLowercase.match(/-/g) || []).length;
+
+    const invalidCharacters = RegExp(/[^\w :-]|_/g);
+
+    // check for invalid charaters (anything other than alpha numerics, spaces, colon and hyphen)
+    if (invalidCharacters.test(referenceLowercase)) {
+      return INVALID_QUERY;
+    }
+
+    const bookName =
+      Object.keys(bookCodes).find((name) =>
+        referenceLowercase.startsWith(name)
+      ) ?? '';
+
+    const hasValidBookName = bookName.length > 0;
+
+    if (!hasValidBookName) {
+      return INVALID_QUERY;
+    }
+
+    const bookCode = bookCodes[bookName];
+
+    const hasValidBookCode =
+      typeof bookCode === 'string' && bookCode.length === 3;
+
+    if (!hasValidBookCode) {
+      return INVALID_QUERY;
+    }
+
+    const [, chapterAndVerse] = referenceLowercase.split(bookName);
+
+    // remove all spaces
+    const chapterAndVerseNoSpaces = chapterAndVerse.replace(/\s/g, '');
+
+    if (colons === 0 && hyphens === 0) {
+      // Genesis 3
+      // bookCode = GEN
+      // chapterAndVerseNoSpaces = 3
+
+      return {
+        youVersionUri: `https://www.bible.com/bible/111/${bookCode}.${chapterAndVerseNoSpaces}.NIV`,
+        queryString: `${bookCode}.${chapterAndVerseNoSpaces}`,
+      };
+    } else if (colons === 0 && hyphens === 1) {
+      // Genesis 3-4
+      // bookCode = GEN
+      // chapterAndVerseNoSpaces = 3-4
+
+      const [startChapter, endChapter] = chapterAndVerseNoSpaces.split('-');
+
+      return {
+        youVersionUri: `https://www.bible.com/bible/111/${bookCode}.${startChapter}.NIV`,
+        queryString: `${bookCode}.${startChapter}-${bookCode}.${endChapter}`,
+      };
+    } else if (colons === 1 && hyphens === 0) {
+      // Genesis 3:1
+      // bookCode = GEN
+      // chapterAndVerseNoSpaces = 3:1
+
+      const chapterAndVerseFormatted = chapterAndVerseNoSpaces.replace(
+        ':',
+        '.'
+      );
+
+      return {
+        youVersionUri: `https://www.bible.com/bible/111/${bookCode}.${chapterAndVerseFormatted}.NIV`,
+        queryString: `${bookCode}.${chapterAndVerseFormatted}`,
+      };
+    } else if (colons === 1 && hyphens === 1) {
+      // Genesis 3:1-2
+      // bookCode = GEN
+      // chapterAndVerseNoSpaces = 3:1-2
+
+      const [chapter, verses] = chapterAndVerseNoSpaces.split(':'); // ['3', '1-2']
+      const [startVerse, endVerse] = verses.split('-'); // ['1', '2']
+      const chapterAndVerseFormatted = chapterAndVerseNoSpaces.replace(
+        ':',
+        '.'
+      );
+
+      return {
+        youVersionUri: `https://www.bible.com/bible/111/${bookCode}.${chapterAndVerseFormatted}.NIV`,
+        queryString: `${bookCode}.${chapter}.${startVerse}-${bookCode}.${chapter}.${endVerse}`,
+      };
+    } else if (colons === 2 && hyphens === 1) {
+      // Genesis 3:20-4:20
+      // bookCode = GEN
+      // chapterAndVerseNoSpaces = 3:20-4:20
+
+      const [startChapterVerse, endChapterVerse] =
+        chapterAndVerseNoSpaces.split('-'); // ['3:20', '4:20']
+
+      const [startChapter, startVerse] = startChapterVerse.split(':'); // ['3', '20']
+      const [endChapter, endVerse] = endChapterVerse.split(':'); // ['4', '20']
+
+      return {
+        youVersionUri: `https://www.bible.com/bible/111/${bookCode}.${startChapter}.NIV`,
+        queryString: `${bookCode}.${startChapter}.${startVerse}-${bookCode}.${endChapter}.${endVerse}`,
+      };
+    } else {
+      return INVALID_QUERY;
+    }
+  } catch (e) {
+    console.error(e);
+    return INVALID_QUERY;
+  }
+};
+
+export const parseJSON = (raw: RawDraftContentState): DataFromJSON => {
+  const blocks = raw.blocks;
+  const entityMap = raw.entityMap;
+
+  const verses: DataFromJSON = [];
+
+  for (const block of blocks) {
+    if (block.entityRanges.length > 0) {
+      block.entityRanges.forEach((entity) => {
+        const data = entityMap[entity.key];
+
+        if (data.type === 'LINK' && data.data.url.includes('biblegateway')) {
+          verses.push({
+            passageRef: block.text.slice(
+              entity.offset,
+              entity.offset + entity.length
+            ),
+            key: block.key,
+            offset: entity.offset,
+            length: entity.length,
+          });
+        }
+      });
+    }
+  }
+
+  return verses;
+};
