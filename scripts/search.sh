013187aa632671e16ab4e71427291da6066d7dbb
George Bell <47749730+GeorgeBellTMH@users.noreply.github.com>
2022-07-13 11:47:30 -0700

    ensure new node

--- scripts/search.sh
+++ scripts/search.sh
@@ -1,41 +1,67 @@
-python3 searchable.py \
-  --rn 'us-east-1' \
-  --tn 'F1ListGroup2-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
-  --lf 'arn:aws:lambda:us-east-1:119330689042:function:DdbToEsFn-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
-  --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/F1ListGroup2-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-11-08T05:35:31.616' \
-  --ak 'AKIAJHPFLPV67FENRH4A' \
-  --sk 'daJCCEAXTDn/wZzeE3s4bzb7ZCERjTIzEaD8CRFY'
+//python3 searchable.py \
+//  --rn 'us-east-1' \
+//  --tn 'F1ListGroup2-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+//  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
+//  --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/F1ListGroup2-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-11-08T05:35:31.616' \
+//  --ak %1 \
+//  --sk %2
 
 python3 searchable.py \
   --rn 'us-east-1' \
   --tn 'Series-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
-  --lf 'arn:aws:lambda:us-east-1:119330689042:function:DdbToEsFn-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
   --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/Series-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2019-10-31T08:25:08.517'\
-  --ak 'AKIAJHPFLPV67FENRH4A' \
-  --sk 'daJCCEAXTDn/wZzeE3s4bzb7ZCERjTIzEaD8CRFY'
+  --ak $1 \
+  --sk $2
 
 python3 searchable.py \
   --rn 'us-east-1' \
   --tn 'CustomPlaylist-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
-  --lf 'arn:aws:lambda:us-east-1:119330689042:function:DdbToEsFn-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
   --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/CustomPlaylist-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-06-11T18:42:16.838'\
-  --ak 'AKIAJHPFLPV67FENRH4A' \
-  --sk 'daJCCEAXTDn/wZzeE3s4bzb7ZCERjTIzEaD8CRFY'
+  --ak $1 \
+  --sk $2
 
 python3 searchable.py \
   --rn 'us-east-1' \
   --tn 'BlogSeries-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
-  --lf 'arn:aws:lambda:us-east-1:119330689042:function:DdbToEsFn-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
   --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/BlogSeries-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-05-19T01:54:24.378'\
-  --ak 'AKIAJHPFLPV67FENRH4A' \
-  --sk 'daJCCEAXTDn/wZzeE3s4bzb7ZCERjTIzEaD8CRFY'
+  --ak $1 \
+  --sk $2
 
 python3 searchable.py \
   --rn 'us-east-1' \
   --tn 'Notes-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
-  --lf 'arn:aws:lambda:us-east-1:119330689042:function:DdbToEsFn-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
   --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/Notes-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-05-28T05:59:07.982'\
-  --ak 'AKIAJHPFLPV67FENRH4A' \
-  --sk 'daJCCEAXTDn/wZzeE3s4bzb7ZCERjTIzEaD8CRFY'
+  --ak $1 \
+  --sk $2
 
 
+python3 searchable.py \
+  --rn 'us-east-1' \
+  --tn 'Comment-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
+  --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/Comment-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-05-28T05:59:07.982'\
+  --ak $1 \
+  --sk $2
+
+
+python3 searchable.py \
+  --rn 'us-east-1' \
+  --tn 'Blog-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
+  --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/Blog-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-05-28T05:59:07.982'\
+  --ak $1 \
+  --sk $2
+
+
+
+python3 searchable.py \
+  --rn 'us-east-1' \
+  --tn 'Video-p6zimwgiwnaqrdq332hf65exye-tmhprod' \
+  --lf 'arn:aws:lambda:us-east-1:119330689042:function:themeetinghouse-tmhprod-2-OpenSearchStreamingLambd-CH2lMsU5IUuR' \
+  --esarn 'arn:aws:dynamodb:us-east-1:119330689042:table/Video-p6zimwgiwnaqrdq332hf65exye-tmhprod/stream/2020-05-28T05:59:07.982'\
+  --ak $1 \
+  --sk $2
