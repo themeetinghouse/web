bf6045d9b63461f98c3c446c53f1fc78759dccd7
Igor Belem <igortbelem@gmail.com>
2023-04-24 12:38:20 -0400

    Series image now points to content S3 bucket

--- src/graphql-custom/customQueries.ts
+++ src/graphql-custom/customQueries.ts
@@ -347,6 +347,14 @@ export const getSeriesBySeriesType = `query GetSeriesBySeriesType(
   ) {
     items {
       id
+      bannerImage { 
+        src
+        alt
+      }
+      babyHeroImage { 
+        src
+        alt
+      }
       videos {
         items {
           id
@@ -400,6 +408,14 @@ export const searchSeries = /* GraphQL */ `
       items {
         id
         seriesType
+        bannerImage {
+          src
+          alt
+        }
+        babyHeroImage {
+          src
+          alt
+        }
         title
         description
         image
