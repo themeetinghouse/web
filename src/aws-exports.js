f3ed06a6dbd893014fa04e4440738a7b36e8dfb5
Igor Belem <igortbelem@gmail.com>
2024-05-12 09:27:51 -0400

    Updated API key (#1268)

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,6 +3,43 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
+    "aws_mobile_analytics_app_id": "0a2b4c0b6e7448249fac4bf70c54b478",
+    "aws_mobile_analytics_app_region": "us-east-1",
+    "Analytics": {
+        "AWSPinpoint": {
+            "appId": "0a2b4c0b6e7448249fac4bf70c54b478",
+            "region": "us-east-1"
+        }
+    },
+    "Notifications": {
+        "Push": {
+            "AWSPinpoint": {
+                "appId": "0a2b4c0b6e7448249fac4bf70c54b478",
+                "region": "us-east-1"
+            }
+        }
+    },
+    "aws_cloud_logic_custom": [
+        {
+            "name": "AdminQueries",
+            "endpoint": "https://903cegfkk8.execute-api.us-east-1.amazonaws.com/prodnew",
+            "region": "us-east-1"
+        },
+        {
+            "name": "image",
+            "endpoint": "https://iq31kud6zk.execute-api.us-east-1.amazonaws.com/prodnew",
+            "region": "us-east-1"
+        },
+        {
+            "name": "stripeAPI",
+            "endpoint": "https://upud6cyeo4.execute-api.us-east-1.amazonaws.com/prodnew",
+            "region": "us-east-1"
+        }
+    ],
+    "aws_appsync_graphqlEndpoint": "https://ivmnti3f45d2phjrvkx62mxgyi.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-e4tke5ydc5hffgrwy5e36qdrmu",
     "aws_cognito_identity_pool_id": "us-east-1:3ff56cd7-e4b2-4543-aed6-94f258a95dd8",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_SuPgaibT1",
@@ -30,46 +67,9 @@ const awsmobile = {
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
     ],
-    "aws_appsync_graphqlEndpoint": "https://ivmnti3f45d2phjrvkx62mxgyi.appsync-api.us-east-1.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-e4tke5ydc5hffgrwy5e36qdrmu",
-    "aws_cloud_logic_custom": [
-        {
-            "name": "image",
-            "endpoint": "https://iq31kud6zk.execute-api.us-east-1.amazonaws.com/prodnew",
-            "region": "us-east-1"
-        },
-        {
-            "name": "AdminQueries",
-            "endpoint": "https://903cegfkk8.execute-api.us-east-1.amazonaws.com/prodnew",
-            "region": "us-east-1"
-        },
-        {
-            "name": "stripeAPI",
-            "endpoint": "https://upud6cyeo4.execute-api.us-east-1.amazonaws.com/prodnew",
-            "region": "us-east-1"
-        }
-    ],
     "aws_content_delivery_bucket": "heeetingouse-20190312104205-hostingbucket-prodnew",
     "aws_content_delivery_bucket_region": "us-east-1",
     "aws_content_delivery_url": "https://dx0d3ykzhvif.cloudfront.net",
-    "aws_mobile_analytics_app_id": "0a2b4c0b6e7448249fac4bf70c54b478",
-    "aws_mobile_analytics_app_region": "us-east-1",
-    "Analytics": {
-        "AWSPinpoint": {
-            "appId": "0a2b4c0b6e7448249fac4bf70c54b478",
-            "region": "us-east-1"
-        }
-    },
-    "Notifications": {
-        "Push": {
-            "AWSPinpoint": {
-                "appId": "0a2b4c0b6e7448249fac4bf70c54b478",
-                "region": "us-east-1"
-            }
-        }
-    },
     "aws_user_files_s3_bucket": "themeetinghouse-usercontent221608-prodnew",
     "aws_user_files_s3_bucket_region": "us-east-1"
 };
